8f1d3190aaa63e0c4ff474cf1f0b9895aab5db45
Or Shachar <ors@wix.com>
2018-01-07 14:39:36 +0200

    managed deps from wix-base-parent-ng instead of wix-master-parent
    GitOrigin-RevId: cd847272debeadd6d5304af564784411be167fef

--- migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/MigratorApp.scala
+++ migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/MigratorApp.scala
@@ -19,7 +19,7 @@ trait MigratorApp extends App {
   def codotaToken = configuration.codotaToken
   lazy val codotaDependencyAnalyzer = new CodotaDependencyAnalyzer(repoRoot, codeModules, codotaToken)
   val thirdPartyDependencySource = Coordinates.deserialize("com.wixpress.common:third-party-dependencies:pom:100.0.0-SNAPSHOT")
-  val managedDependenciesArtifact = Coordinates.deserialize("com.wixpress.common:wix-master-parent:pom:100.0.0-SNAPSHOT")
+  val managedDependenciesArtifact = Coordinates.deserialize("com.wixpress.common:wix-base-parent-ng:pom:100.0.0-SNAPSHOT")
 
   private def staleFactorInHours = sys.props.getOrElse("num.hours.classpath.cache.is.fresh","24").toInt
 
