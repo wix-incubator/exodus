0c465eb6193c92f561ef494d9ad37b4720ed6651
Igal Tabachnik <igalt@wix.com>
2018-01-15 15:27:35 +0200

    Adding maven build groovy script
    GitOrigin-RevId: 896dac4811116a286c1edf0549b818c56f03e6cd

---
+++ migrator/scripts/run-maven.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+    options {
+        timestamps()
+    }
+    tools {
+        maven 'M3'
+    }
+    environment {
+        MAVEN_INSTALL = "mvn clean install -B -Dwix.environment=CI"
+        REPO_NAME = find_repo_name()
+    }
+    stages {
+        stage('checkout') {
+            steps {
+                dir("${env.REPO_NAME}") {
+                    git "${env.repo_url}"
+                }
+            }
+        }
+        stage('mvn') {
+            steps {
+                sh "${env.MAVEN_INSTALL}"
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts "**/target/**/TEST-*.xml"
+        }
+        success{
+            build job: "01-run-bazel", propagate: false, wait: false
+        }
+    }
+}
+
+@SuppressWarnings("GroovyAssignabilityCheck")
+def find_repo_name() {
+    name = "${env.repo_url}".split('/')[-1]
+    if (name.endsWith(".git"))
+        name = name[0..-5]
+    return name
+}
