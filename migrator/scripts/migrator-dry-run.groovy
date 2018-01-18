f7b0080e83b60e718c8c228d6c4527323a8fd11e
Or Shachar <ors@wix.com>
2018-01-18 16:05:15 +0200

    fixed dry run
    GitOrigin-RevId: 38541c687e438cd09ddac903d6ca33100821a766

--- migrator/scripts/migrator-dry-run.groovy
+++ migrator/scripts/migrator-dry-run.groovy
@@ -30,16 +30,14 @@ pipeline {
                 sh 'rm -rf third_party'
                 sh 'find . -path "*/*BUILD" -exec rm -f {} \\;'
                 sh "java -Xmx12G -Dcodota.token=${env.CODOTA_TOKEN} -Dclean.codota.analysis.cache=true -Dskip.classpath=false -Dskip.transformation=false -Dfail.on.severe.conflicts=true -Drepo.root=. -jar wix-bazel-migrator-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
-            }
-        }
-        stage('buildifier') {
-            steps {
                 sh "buildozer 'add tags manual' //third_party/...:%scala_import"
                 sh 'buildifier $(find . -iname BUILD -type f)'
             }
         }
         stage('build') {
-            sh "bazel build --strategy=Scalac=worker //..."
+            stepes {
+                sh "bazel build --strategy=Scalac=worker //..."
+            }
         }
         stage('UT') {
             steps {
