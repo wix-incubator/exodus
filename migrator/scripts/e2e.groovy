de590262403231d71a71f9ba27ef20faadae2071
Igal Tabachnik <igalt@wix.com>
2018-01-21 15:07:50 +0200

    Adding e2e script
    GitOrigin-RevId: 4266dd3014a2d0ec71b99c6bd13ae237f51e3dfc

---
+++ migrator/scripts/e2e.groovy
@@ -0,0 +1,49 @@
+def migration_branch = null
+def bazel_success = false
+def maven_success = false
+
+pipeline {
+    agent any
+    options {
+        timestamps()
+    }
+    stages {
+        stage('migrate') {
+            steps {
+                script {
+                    def migrate_run = build job: "01-migrate", wait: true, propagate: true
+                    migration_branch = "bazel-mig-${migrate_run.number}"
+                }
+            }
+        }
+        stage('build-and-test') {
+            steps {
+                parallel(
+                        "bazel": {
+                            script {
+                                def b = build job: "02-run-bazel", wait: true, propagate: true, parameters: [string(name: 'BRANCH_NAME', value: migration_branch), booleanParam(name: 'CLEAN', value: false)]
+                                bazel_success = (b.result == "SUCCESS") || (b.result == "UNSTABLE")
+                            }
+                        },
+                        "maven": {
+                            script {
+                                def m = build job: "02-run-maven", wait: true, propagate: true, parameters: [string(name: 'BRANCH_NAME', value: migration_branch), booleanParam(name: 'CLEAN', value: false)]
+                                maven_success = (m.result == "SUCCESS") || (m.result == "UNSTABLE")
+                            }
+                        }
+                )
+            }
+        }
+        stage('compare') {
+            steps {
+                script {
+                    if (bazel_success && maven_success) {
+                        build job: "04-compare", wait: true
+                    } else {
+                        error("bazel run or maven run failed")
+                    }
+                }
+            }
+        }
+    }
+}
