6f22606559e8dc209d9649d351155aedd71dc6c2
Or Shachar <or.shachar@mail.huji.ac.il>
2018-02-26 18:04:14 +0200

    doubled migration timeout
    GitOrigin-RevId: 9a8d605f73a1b0d6dc786089ed848ea7f15684e4

--- migrator/scripts/migrate.groovy
+++ migrator/scripts/migrate.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     options {
-        timeout(time: 80, unit: 'MINUTES')
+        timeout(time: 160, unit: 'MINUTES')
         timestamps()
         throttle(categories: ['migrate'])
     }
