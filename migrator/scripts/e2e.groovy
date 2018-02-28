24b14c81bef669ed8eaedff6581025994cc22434
Or Shachar <or.shachar@mail.huji.ac.il>
2018-02-28 11:35:10 +0200

    fixed e2e groovy (#87)
    GitOrigin-RevId: 37c541ebad25a65a9e7a61d109446793d8443cf9

--- migrator/scripts/e2e.groovy
+++ migrator/scripts/e2e.groovy
@@ -1,5 +1,3 @@
-def migration_branch = null
-def bazel_success = false
 def maven_success = false
 
 pipeline {
@@ -18,47 +16,42 @@ pipeline {
                 }
             }
         }
-        stage('run-migration-e2e') {
-            steps {
-                parallel(
-                        "migrate": {
-                            script {
-                                println "Commit hash: ${env.GIT_COMMIT_HASH}"
-                                def migrate_run = build job: "01-migrate", wait: true, propagate: true, parameters: [booleanParam(name: 'TRIGGER_BUILD', value: false), string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")]
-                                migration_branch = "bazel-mig-${migrate_run.number}"
-                                BUILD_PARAMETERS = [
-                                        string(name: 'BRANCH_NAME', value: migration_branch),
-                                        booleanParam(name: 'CLEAN', value: false),
-                                        string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")
-                                ]
-                            }
-                        },
-                        "maven": {
-                            script {
-                                def m = build job: "02-run-maven", wait: true, propagate: false, parameters: [string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")]
-                                maven_success = (m.result == "SUCCESS") || (m.result == "UNSTABLE")
-                            }
+        stage('bazel-and-maven') {
+            parallel {
+                stage('bazel') {
+                    steps {
+                        script {
+                            println "Commit hash: ${env.GIT_COMMIT_HASH}"
+                            def migrate_run = build job: "01-migrate", wait: true, propagate: true, parameters: [booleanParam(name: 'TRIGGER_BUILD', value: false), string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")]
+                            def migration_branch = "bazel-mig-${migrate_run.number}"
+                            def parameters = [
+                                    string(name: 'BRANCH_NAME', value: migration_branch),
+                                    booleanParam(name: 'CLEAN', value: false),
+                                    string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")
+                            ]
+                            build job: "03-fix-strict-deps", wait: true, propagate: false, parameters: parameters
+                            build job: "05-run-bazel-rbe", wait: false, propagate: false, parameters: parameters
+                            build job: "02-run-bazel", wait: true, propagate: false, parameters: parameters
                         }
-                )
-            }
-        }
-        stage('build-and-test') {
-            steps {
-                script {
-                    build job: "03-fix-strict-deps", wait: true, propagate: false, parameters: env.BUILD_PARAMETERS
-                    build job: "05-run-bazel-rbe", wait: false, propagate: false, parameters: env.BUILD_PARAMETERS
-                    def b = build job: "02-run-bazel", wait: true, propagate: false, parameters: env.BUILD_PARAMETERS
-                    bazel_success = (b.result == "SUCCESS") || (b.result == "UNSTABLE")
+                    }
+                }
+                stage('maven') {
+                    steps {
+                        script {
+                            def m = build job: "02-run-maven", wait: true, propagate: false, parameters: [string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")]
+                            maven_success = (m.result == "SUCCESS") || (m.result == "UNSTABLE")
+                        }
+                    }
                 }
             }
         }
         stage('compare') {
             steps {
                 script {
-                    if (bazel_success && maven_success) {
+                    if (maven_success) {
                         build job: "03-compare", wait: true
                     } else {
-                        error("bazel run or maven run failed")
+                        error("maven run failed - cannot compare")
                     }
                 }
             }
