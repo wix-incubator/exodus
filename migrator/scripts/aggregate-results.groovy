d4c27d826ccdb0a9ad05216d2984d4683f15588e
Igal Tabachnik <igalt@wix.com>
2018-03-04 17:46:59 +0200

    Adding totals to aggregate report
    GitOrigin-RevId: 9968343044aa8151892ec9319233755942a513a5

--- migrator/scripts/aggregate-results.groovy
+++ migrator/scripts/aggregate-results.groovy
@@ -10,7 +10,6 @@ node {
     def compare_job_name = "/03-compare"
     def remote_job_name = "/05-run-bazel-rbe"
 
-
     def compare_success = 0
     def compare_fail = 0
     def compare_never_run = 0
@@ -24,6 +23,8 @@ node {
     def migrate_failure = 0
     def migrate_never_run = 0
 
+    def compare_total_maven = 0
+    def compare_total_bazel = 0
     def compare_same = 0
     def compare_different = 0
     def compare_missing = 0
@@ -34,6 +35,11 @@ node {
     def remote_run_unstable = 0
     def remote_run_never_run = 0
 
+    def maven_tests = 0
+    def bazel_tests = 0
+    def migrated_tests = 0
+    def compiled_tests = 0
+    def count = 1
 
     def recent = { run ->
         if (run == null)
@@ -49,15 +55,27 @@ node {
         def migrate_run = Jenkins.instance.getItemByFullName(it + migrate_job_name).lastCompletedBuild
         def bazel_remote_run = Jenkins.instance.getItemByFullName(it + remote_job_name).lastCompletedBuild
 
+        def migrated = false
+        def compiled = false
+        def bazel_run = false
 
         if (recent(compare_run)) {
             if (compare_run.result == Result.SUCCESS)
-                compare_success = compare_success + 1
+                compare_success += 1
             else
-                compare_fail = compare_fail + 1
+                compare_fail += 1
 
             def log = compare_run.log
-            def match = log =~ /same: (\d+), different: (\d+), missing: (\d+), overridden: (\d+)/
+            def match = log =~ /> maven cases: (\d+)/
+            if (match.size() > 0) {
+                maven_tests = match[0][1].toInteger()
+            }
+            match = log =~ /> bazel cases: (\d+)/
+            if (match.size() > 0) {
+                bazel_tests = match[0][1].toInteger()
+            }
+
+            match = log =~ /same: (\d+), different: (\d+), missing: (\d+), overridden: (\d+)/
             try {
                 if (match.size() > 0) {
                     compare_same       += match[0][1].toInteger()
@@ -73,32 +91,41 @@ node {
         }
 
         if (recent(bazel_run_run)) {
-            if (bazel_run_run.result == Result.SUCCESS)
-                run_success = run_success + 1
-            else if (bazel_run_run.result == Result.UNSTABLE)
-                run_unstable = run_unstable + 1
-            else
-                run_failure = run_failure + 1
+            if (bazel_run_run.result == Result.SUCCESS) {
+                bazel_run = true
+                run_success += 1
+            } else if (bazel_run_run.result == Result.UNSTABLE) {
+                compiled = true
+                run_unstable += 1
+            } else
+                run_failure += 1
         } else
-            run_never_run = run_never_run + 1
+            run_never_run += 1
 
         if (recent(migrate_run)) {
-            if (migrate_run.result == Result.SUCCESS)
-                migrate_success = migrate_success + 1
-            else
-                migrate_failure = migrate_failure + 1
+            if (migrate_run.result == Result.SUCCESS) {
+                migrated = true
+                migrate_success += 1
+            } else
+                migrate_failure += 1
         } else
-            migrate_never_run = migrate_never_run + 1
+            migrate_never_run += 1
 
         if (recent(bazel_remote_run)) {
             if (bazel_remote_run.result == Result.SUCCESS)
-                remote_run_success = remote_run_success + 1
+                remote_run_success += 1
             else if (bazel_remote_run.result == Result.UNSTABLE)
-                remote_run_unstable = remote_run_unstable + 1
+                remote_run_unstable += 1
             else
-                remote_run_fail = remote_run_fail + 1
+                remote_run_fail += 1
         } else
-            remote_run_never_run = remote_run_never_run + 1
+            remote_run_never_run += 1
+
+        compare_total_maven += maven_tests
+        compare_total_bazel += bazel_run ? bazel_tests : 0
+        migrated_tests += migrated ? maven_tests : 0
+        compiled_tests += compiled ? maven_tests : 0
+        count++
     }
     def res =  """```
     |Total ${folders.size}
@@ -119,11 +146,11 @@ node {
     | - failure = ${compare_fail}
     | - not-run = ${compare_never_run}
     | -----
-    | - total tests in maven: 
-    | - total same = ${compare_same}
-    | - total different = ${compare_different}
-    | - total missing = ${compare_missing}
-    | - total overridden = ${compare_overridden}
+    | TOTALS (WIP, don't trust these numbers yet)
+    | - # Maven = ${compare_total_maven} (in ${count} projects)
+    | - # Migrated = ${migrated_tests}
+    | - # Compiled = ${compiled_tests}
+    | - # Bazel = ${compare_total_bazel}
     |--
     |REMOTE
     | - success = ${remote_run_success}
@@ -133,10 +160,9 @@ node {
     |```""".stripMargin()
 
     echo res
-    // slackSend channel: "#bazel-mig-reports", message: res
+    slackSend channel: "#bazel-mig-reports", message: res
 
 }
-
 
 @NonCPS
 def all_folders() {
