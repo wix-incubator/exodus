60c7d6b5635b7c07cf0c159811de6058bfccc0b2
alonbl <alonbl@wix.com>
2018-03-15 18:12:01 +0200

    updating rbe container
    GitOrigin-RevId: 1e08eb74cd40a8834a8adcf66b09d6329531da7f

--- migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/BazelRcRemoteWriter.scala
+++ migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/BazelRcRemoteWriter.scala
@@ -40,7 +40,7 @@ class BazelRcRemoteWriter(repoRoot: File) {
        |build:remote --host_javabase=@core_server_build_tools//rbe-toolchains/jdk:jdk8
        |build:remote --javabase=@core_server_build_tools//rbe-toolchains/jdk:jdk8
        |build:remote --crosstool_top=@bazel_toolchains//configs/debian8_clang/0.3.0/bazel_0.10.0:toolchain
-       |build:remote --experimental_remote_platform_override='properties:{ name:"container-image" value:"docker://gcr.io/gcb-with-custom-workers/rbe-toolchain-container@sha256:89a4c3784b31a379ec215888e8a62dec809a48c5a26b5b141c0779bed0ec589b" }'
+       |build:remote --experimental_remote_platform_override='properties:{ name:"container-image" value:"docker://gcr.io/gcb-with-custom-workers/rbe-toolchain-container@sha256:f858c30c627fcde6e7db069ad454bce720994a61731b1f1fa4647d48a5edb341" }'
        |
        |# Set various strategies so that all actions execute remotely. Mixing remote
        |# and local execution will lead to errors unless the toolchain and remote
