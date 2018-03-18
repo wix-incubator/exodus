0cc11b93ec761cc87cd1a36e654c5b62d31390c4
Or Shachar <or.shachar@mail.huji.ac.il>
2018-03-18 18:36:16 +0200

    Revert "Fixing e2e script to work like the uber one" (#128)
    This reverts commit 8736270022adeef922c276ad8f05457166ad6cbe.
    
    GitOrigin-RevId: 25b8ed5533244868d90f821c55aa003ada21d722

--- migrator/scripts/e2e.groovy
+++ migrator/scripts/e2e.groovy
@@ -9,32 +9,51 @@ pipeline {
         LATEST_COMMIT_HASH_COMMAND = "git ls-remote -q ${env.repo_url} | head -1 | cut -f 1"
     }
     stages {
-        stage('run-e2e') {
-            echo "sleeping ${sleep_time}"
-            def git_commit_hash = sh(script: "git ls-remote -q ${repo} | head -1 | cut -f 1", returnStdout: true)
-            parallel(
-                    "bazel": {
-                        echo "Git commit hash: ${git_commit_hash}"
-                        def migrate_run = build job: migrate, wait: true, propagate: false,
-                                parameters: [booleanParam(name: 'TRIGGER_BUILD', value: false), string(name: 'COMMIT_HASH', value: git_commit_hash)], quietPeriod: sleep_time
-                        if (migrate_run.result == "SUCCESS") {
-                            def migration_branch = "bazel-mig-${migrate_run.number}"
-                            def parameters = [
-                                    string(name: 'BRANCH_NAME', value: migration_branch),
-                                    booleanParam(name: 'CLEAN', value: false),
-                                    string(name: 'COMMIT_HASH', value: git_commit_hash)
-                            ]
-                            build job: fix_deps, wait: true, propagate: false, parameters: parameters + booleanParam(name: 'TRIGGER_BUILD', value: false)
-                            build job: run_rbe, wait: false, propagate: false, parameters: parameters
-                            build job: run_bazel, wait: true, propagate: false, parameters: parameters
+        stage('setup') {
+            steps {
+                script {
+                    env.GIT_COMMIT_HASH = sh(script: "${env.LATEST_COMMIT_HASH_COMMAND}", returnStdout: true)
+                }
+            }
+        }
+        stage('bazel-and-maven') {
+            parallel {
+                stage('bazel') {
+                    steps {
+                        script {
+                            println "Commit hash: ${env.GIT_COMMIT_HASH}"
+                            def migrate_run = build job: "01-migrate", wait: true, propagate: false, parameters: [booleanParam(name: 'TRIGGER_BUILD', value: false), string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")]
+                            if (migrate_run.result == "SUCCESS") {
+                                def migration_branch = "bazel-mig-${migrate_run.number}"
+                                def parameters = [
+                                        string(name: 'BRANCH_NAME', value: migration_branch),
+                                        booleanParam(name: 'TRIGGER_BUILD', value: false),
+                                        booleanParam(name: 'CLEAN', value: false),
+                                        string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")
+                                ]
+                                build job: "03-fix-strict-deps", wait: true, propagate: false, parameters: parameters
+                                build job: "05-run-bazel-rbe", wait: false, propagate: false, parameters: parameters
+                                build job: "02-run-bazel", wait: true, propagate: false, parameters: parameters
+                            }
                         }
-                    },
-                    "maven": {
-                        build job: run_maven, wait: true, propagate: false,
-                                parameters: [string(name: 'COMMIT_HASH', value: git_commit_hash), booleanParam(name: 'CLEAN', value: false)], quietPeriod: sleep_time
                     }
-            )
-            build job: compare, wait: false
+                }
+                stage('maven') {
+                    steps {
+                        script {
+                            def m = build job: "02-run-maven", wait: true, propagate: false, parameters: [string(name: 'COMMIT_HASH', value: "${env.GIT_COMMIT_HASH}")]
+                            maven_success = (m.result == "SUCCESS") || (m.result == "UNSTABLE")
+                        }
+                    }
+                }
+            }
+        }
+        stage('compare') {
+            steps {
+                script {
+                    build job: "03-compare", wait: true, parameters: [booleanParam(name: 'MAVEN_SUCCESS', value: maven_success)]
+                }
+            }
         }
     }
 }
