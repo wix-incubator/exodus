7df494afa27213f5bfa27e4f6b6c82301ddf07d4
Alon Bar Lev <barlevalon@users.noreply.github.com>
2018-03-29 11:25:57 +0300

    Revert "changing aggregate target separator from + to _ (rbe issue)"
    GitOrigin-RevId: 8af2d2ffbd093e7fb8aaf6e8f42d5f808cb25928

--- migrator/bazel-migrator/src/test/java/com/wix/bazel/migrator/transform/TransformerAcceptanceTest.scala
+++ migrator/bazel-migrator/src/test/java/com/wix/bazel/migrator/transform/TransformerAcceptanceTest.scala
@@ -289,7 +289,7 @@ class TransformerAcceptanceTest extends SpecificationWithJUnit {
 
       val packages = transformer.transform(repo.modules)
 
-      packages must contain(exactly(aPackage(target = aTarget(name = s"agg=lib${aggregateSeparator}lib2"))))
+      packages must contain(exactly(aPackage(target = aTarget(name = "agg=lib+lib2"))))
     }
 
     "externalize originating source module for jvm targets to allow scope track-back" in new Context {
@@ -554,9 +554,7 @@ class TransformerAcceptanceTest extends SpecificationWithJUnit {
         (_: Target.Resources).codePurpose aka "code purpose"
       }
 
-  def aggregateSeparator = "_"
-
-  def aggregatorOf(targets: String*): String = "agg=" + targets.mkString(aggregateSeparator)
+  def aggregatorOf(targets: String*): String = "agg=" + targets.mkString("+")
 
   def test(testType: TestType): Matcher[CodePurpose] = be_===(CodePurpose.Test(testType))
 }
