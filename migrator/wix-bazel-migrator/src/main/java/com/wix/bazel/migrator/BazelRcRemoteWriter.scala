a5c447ad2e094ab5113fc12eb951cb4801d1810a
Alon Bar Lev <barlevalon@users.noreply.github.com>
2018-05-07 18:02:00 +0300

    rbe container that has gcloud (for restaurants) (#238)
    GitOrigin-RevId: e133364b45c0df1b24e8e99bc5b867913de47bcb

--- migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/BazelRcRemoteWriter.scala
+++ migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/BazelRcRemoteWriter.scala
@@ -39,7 +39,7 @@ class BazelRcRemoteWriter(repoRoot: Path) {
        |build:remote --host_javabase=@core_server_build_tools//rbe-toolchains/jdk:jdk8
        |build:remote --javabase=@core_server_build_tools//rbe-toolchains/jdk:jdk8
        |build:remote --crosstool_top=@bazel_toolchains//configs/debian8_clang/0.3.0/bazel_0.13.0/default:toolchain
-       |build:remote --experimental_remote_platform_override='properties:{ name:"container-image" value:"docker://gcr.io/gcb-with-custom-workers/rbe-toolchain-container@sha256:75cddcb5223470d0ecf4aba04973eecddde57249474769ef71655d02febc71c1" }'
+       |build:remote --experimental_remote_platform_override='properties:{ name:"container-image" value:"docker://gcr.io/gcb-with-custom-workers/rbe-toolchain-container@sha256:13825335701b40780639b3ca61d53ef358effb42638e4774ad5401bca92afe66" }'
        |
        |# Set various strategies so that all actions execute remotely. Mixing remote
        |# and local execution will lead to errors unless the toolchain and remote
