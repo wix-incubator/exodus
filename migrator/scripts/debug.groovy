d2825d6e27477326e3dc48fb982a3c861d73e575
Natan Silnitsky <natans@wix.com>
2018-08-02 14:52:31 +0300

    fix script (#402)
    GitOrigin-RevId: 46dd4cfc9d94fd342a6e69b3bb4dc3f79b029f0a

--- migrator/scripts/debug.groovy
+++ migrator/scripts/debug.groovy
@@ -9,6 +9,7 @@ pipeline {
     }
     environment {
         CODOTA_TOKEN = credentials("codota-token")
+        ARTIFACTORY_TOKEN = credentials("artifactory-token")
         REPO_NAME = find_repo_name()
         MANAGED_DEPS_REPO_URL = "git@github.com:wix-private/core-server-build-tools.git"
         MANAGED_DEPS_REPO_NAME = "core-server-build-tools"
@@ -43,6 +44,7 @@ pipeline {
                 dir("wix-bazel-migrator") {
                     sh """|java -Xmx12G \\
                           |   -Dcodota.token=${env.CODOTA_TOKEN} \\
+                          |   -Dartifactory.token=${env.ARTIFACTORY_TOKEN} \\
                           |   -Dskip.classpath=${env.skip_classpath} \\
                           |   -Dskip.transformation=false \\
                           |   -Dmanaged.deps.repo=../${env.MANAGED_DEPS_REPO_NAME} \\
