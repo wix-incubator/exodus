fa57edf49c1c389309ea84ee0236a06154e65c9b
Ittai Zeidman <ittaiz@gmail.com>
2018-08-05 18:54:42 +0300

    DefaultJavaToolchainWriter uses default java toolchain from core_server (#407)
    * DefaultJavaToolchainWriter uses default java toolchain from core_server_build_tools
    
    * Removing JavacOptsIT since it now needs core-server-build-tools
    Currently we don't know how to version git_repository into bazel e2e test
    
    GitOrigin-RevId: b2e0cf5499f0d3793349913b926bf1ec91774ee4

--- migrator/wix-bazel-migrator/src/it/java/com/wix/bazel/migrator/DefaultJavaToolchainWriterIT.scala
+++ migrator/wix-bazel-migrator/src/it/java/com/wix/bazel/migrator/DefaultJavaToolchainWriterIT.scala
@@ -4,41 +4,6 @@ import java.nio.file.Files
 
 class DefaultJavaToolchainWriterIT extends BaseWriterIT {
   "DefaultJavaToolchainWriter" should {
-    "create BUILD.bazel with default java tool chain content" in new ctx with emptyBazelRcFile {
-      writer.write()
-
-      path(withName = "BUILD.bazel") must beRegularFile(
-        withContent =
-          DefaultJavaToolchainWriter.createDefaultToolchain(DefaultJavaToolchainWriter.DefaultJavacOpts,
-            DefaultJavaToolchainWriter.DefaultJavaToolchainName).split(System.lineSeparator)
-      )
-    }
-
-    "create BUILD.bazel with default java tool chain content with given javacopts" in new ctx with emptyBazelRcFile {
-      val randomJavacopts = Seq(random, random)
-
-      override val writer = new DefaultJavaToolchainWriter(repoRoot, randomJavacopts)
-      writer.write()
-
-      path(withName = "BUILD.bazel") must beRegularFile(
-        withContent = DefaultJavaToolchainWriter.createDefaultToolchain(randomJavacopts,
-          DefaultJavaToolchainWriter.DefaultJavaToolchainName).split(System.lineSeparator)
-      )
-    }
-
-    "append default java tool chain content to BUILD.bazel" in new ctx with emptyBazelRcFile {
-      val randomContent = createRandomFile("BUILD.bazel")
-
-      writer.write()
-
-      path(withName = "BUILD.bazel") must beRegularFile(
-        withContent =
-          (randomContent +
-            DefaultJavaToolchainWriter.createDefaultToolchain(DefaultJavaToolchainWriter.DefaultJavacOpts,
-              DefaultJavaToolchainWriter.DefaultJavaToolchainName)).split(System.lineSeparator)
-      )
-    }
-
     "create .bazelrc file is missing" in new ctx {
       writer.write()
 
@@ -57,13 +22,6 @@ class DefaultJavaToolchainWriterIT extends BaseWriterIT {
   abstract class ctx extends baseCtx {
     val writer = new DefaultJavaToolchainWriter(repoRoot)
 
-    def createRandomFile(path: String) = {
-      val content = random
-      Files.write(repoRoot.resolve(path), content.getBytes)
-
-      content
-    }
-
     def createBazelRcFile(withContent: String = "") = {
       Files.write(repoRoot.resolve(".bazelrc"), withContent.getBytes)
 
@@ -71,7 +29,4 @@ class DefaultJavaToolchainWriterIT extends BaseWriterIT {
     }
   }
 
-  trait emptyBazelRcFile { this: ctx =>
-    createBazelRcFile()
-  }
 }
--- migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/DefaultJavaToolchainWriter.scala
+++ migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/DefaultJavaToolchainWriter.scala
@@ -4,47 +4,19 @@ import java.nio.file.{Files, Path, StandardOpenOption}
 
 import com.wix.bazel.migrator.DefaultJavaToolchainWriter._
 
-class DefaultJavaToolchainWriter(repoRoot: Path, javacopts: Seq[String] = DefaultJavaToolchainWriter.DefaultJavacOpts) {
+class DefaultJavaToolchainWriter(repoRoot: Path) {
   def write(): Unit = {
-    val targetName = DefaultJavaToolchainName
-
-    updateBasePackage(targetName)
-    updateBazelRcFile(targetName)
+    updateBazelRcFile(DefaultJavaToolchainName)
   }
-
-  private def updateBasePackage(targetName: String): Unit =
-    appendOrCreateFile("BUILD.bazel", createDefaultToolchain(javacopts, targetName))
 
   private def updateBazelRcFile(targetName: String): Unit =
     new BazelRcWriter(repoRoot).appendLine(bazelRcToolchainUsage(targetName))
 
-  private def appendOrCreateFile(filename: String, content: String): Unit =
-    Files.write(repoRoot.resolve(filename), content.getBytes, StandardOpenOption.APPEND, StandardOpenOption.CREATE)
 }
 
 object DefaultJavaToolchainWriter {
-  val DefaultJavaToolchainName: String = "wix_default_java_toolchain"
+  val DefaultJavaToolchainName: String = "@core_server_build_tools//toolchains:wix_default_java_toolchain"
 
-  val DefaultJavacOpts: Seq[String] = Seq(
-    "-g",
-    "-deprecation",
-    "-XepDisableAllChecks"
-  )
+  def bazelRcToolchainUsage(targetName: String): String = s"build --java_toolchain=$targetName"
 
-  val LoadDefaultToolchain: String = """load("@bazel_tools//tools/jdk:default_java_toolchain.bzl", "default_java_toolchain")""".stripMargin
-
-  def bazelRcToolchainUsage(targetName: String): String = s"build --java_toolchain=//:$targetName"
-
-  def createDefaultToolchain(javacOpts: Seq[String], targetName: String): String =
-    s"""
-       |$LoadDefaultToolchain
-       |
-       |default_java_toolchain(
-       |  name = "$targetName",
-       |  jvm_opts = [ "-Xbootclasspath/p:$$(location @bazel_tools//third_party/java/jdk/langtools:javac_jar)", ],
-       |  javac = ["@bazel_tools//third_party/java/jdk/langtools:javac_jar",],
-       |  bootclasspath = ["@bazel_tools//tools/jdk:platformclasspath.jar",],
-       |  visibility = ["//visibility:public",],
-       |  misc = [${javacOpts.map(j => s""""$j"""").mkString(", ")}]
-       )""".stripMargin
 }
