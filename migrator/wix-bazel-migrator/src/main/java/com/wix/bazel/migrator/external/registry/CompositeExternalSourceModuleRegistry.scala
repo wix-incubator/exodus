8b20e8d96a3d4260aef7a26cdf1ecb715991c3b9
Or Shachar <ors@wix.com>
2018-08-23 17:52:37 +0300

    Better impl for Composite ExternalSourceModuleRegistry (#494)
    GitOrigin-RevId: c7e851be25225e4e0849c4db5c4eb75af057d39b

--- migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/external/registry/CompositeExternalSourceModuleRegistry.scala
+++ migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/external/registry/CompositeExternalSourceModuleRegistry.scala
@@ -3,6 +3,6 @@ package com.wix.bazel.migrator.external.registry
 class CompositeExternalSourceModuleRegistry(priorityOrderedRegistries:ExternalSourceModuleRegistry*) extends ExternalSourceModuleRegistry {
 
   override def lookupBy(groupId: String, artifactId: String): Option[String] =
-    priorityOrderedRegistries.toStream.map(_.lookupBy(groupId,artifactId)).find(_.isDefined).flatten
+    priorityOrderedRegistries.toStream.flatMap(_.lookupBy(groupId,artifactId)).headOption
 
 }
--- migrator/wix-bazel-migrator/src/test/scala/com/wix/bazel/migrator/external/registry/CompositeExternalSourceModuleRegistryTest.scala
+++ migrator/wix-bazel-migrator/src/test/scala/com/wix/bazel/migrator/external/registry/CompositeExternalSourceModuleRegistryTest.scala
@@ -2,12 +2,13 @@ package com.wix.bazel.migrator.external.registry
 
 import org.specs2.mutable.SpecificationWithJUnit
 
+import scala.util.Try
+
 class CompositeExternalSourceModuleRegistryTest extends SpecificationWithJUnit {
   "ComposeExternalSourceModuleRegistry" should {
     "return the first matching lookup for groupId and artifactId" in {
       val groupId = "g"
       val artifactId = "a"
-
       val location1 = "location1"
       val location2 = "location2"
       val registry1 = new FakeExternalSourceModuleRegistry(Map((groupId, artifactId) -> location1))
@@ -15,7 +16,7 @@ class CompositeExternalSourceModuleRegistryTest extends SpecificationWithJUnit {
 
       val composite = new CompositeExternalSourceModuleRegistry(registry1, registry2)
 
-      composite.lookupBy(groupId,artifactId) must beSome(location1)
+      composite.lookupBy(groupId, artifactId) must beSome(location1)
     }
 
     "return none if none of the registries found matching lookup" in {
@@ -24,6 +25,20 @@ class CompositeExternalSourceModuleRegistryTest extends SpecificationWithJUnit {
       val composite = new CompositeExternalSourceModuleRegistry(registry1, registry2)
 
       composite.lookupBy("some.group", "some-artifact") must beNone
+    }
+
+    "stop looking for ExternalSourceModule after finding it in the first registry" in {
+      val groupId = "g"
+      val artifactId = "a"
+      val location = "location1"
+      val theGoodRegistry = new FakeExternalSourceModuleRegistry(Map((groupId, artifactId) -> location))
+      val exceptionThrowingRegistry = new ExternalSourceModuleRegistry {
+        override def lookupBy(groupId: String, artifactId: String): Option[String] = throw new RuntimeException("should not get to this code")
+      }
+
+      val composite = new CompositeExternalSourceModuleRegistry(theGoodRegistry, exceptionThrowingRegistry)
+
+      Try(composite.lookupBy(groupId, artifactId)) must beSuccessfulTry
     }
   }
 }
