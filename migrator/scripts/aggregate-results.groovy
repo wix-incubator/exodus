5f9d43e370707ed85b24a4ea29f0d2947e001308
Igal Tabachnik <hmemcpy@gmail.com>
2018-10-04 11:34:31 +0300

    Aggregating results from bazel rbe and sandbox runs #automerge (#714)
    GitOrigin-RevId: b2ec0827213158d38d2c94be589183ad9537a6be

--- migrator/scripts/aggregate-results.groovy
+++ migrator/scripts/aggregate-results.groovy
@@ -8,12 +8,22 @@ node {
     def migrate_job_name = "/01-migrate"
     def bazel_run_job_name = "/02-run-bazel"
     def compare_job_name = "/03-compare"
+    def compare_rbe_job_name = "/03-compare-rbe"
+    def compare_sandbox_job_name = "/03-compare-sandbox"
     def remote_job_name = "/05-run-bazel-rbe"
     def sandbox_job_name = "/run-bazel-sandboxed"
 
     def compare_success = 0
     def compare_fail = 0
     def compare_never_run = 0
+
+    def compare_success_rbe = 0
+    def compare_fail_rbe = 0
+    def compare_never_run_rbe = 0
+
+    def compare_success_sandbox = 0
+    def compare_fail_sandbox = 0
+    def compare_never_run_sandbox = 0
 
     def run_success = 0
     def run_failure = 0
@@ -26,6 +36,8 @@ node {
 
     def total_maven = 0
     def total_bazel = 0
+    def total_bazel_rbe = 0
+    def total_bazel_sandbox = 0
 
     def remote_run_success = 0
     def remote_run_fail = 0
@@ -44,6 +56,20 @@ node {
     def compare_ignored = 0
     def compare_failed = 0
 
+    def compare_same_rbe = 0
+    def compare_different_rbe = 0
+    def compare_missing_rbe = 0
+    def compare_overridden_rbe = 0
+    def compare_ignored_rbe = 0
+    def compare_failed_rbe = 0
+
+    def compare_same_sandbox = 0
+    def compare_different_sandbox = 0
+    def compare_missing_sandbox = 0
+    def compare_overridden_sandbox = 0
+    def compare_ignored_sandbox = 0
+    def compare_failed_sandbox = 0
+
     def sandbox_run_success = 0
     def sandbox_run_fail = 0
     def sandbox_run_unstable = 0
@@ -55,36 +81,35 @@ node {
     folders.each {
         def maven_tests = 0
 
-        def compare_run      = get_latest_run(it + compare_job_name, latest_uber_job)
-        def bazel_run_run    = get_latest_run(it + bazel_run_job_name, latest_uber_job)
-        def migrate_run      = get_latest_run(it + migrate_job_name, latest_uber_job)
-        def bazel_remote_run = get_latest_run(it + remote_job_name, latest_uber_job)
-        def bazel_sandbox_run = get_latest_run(it + sandbox_job_name, latest_uber_job)
+        def compare_run         = get_latest_run(it + compare_job_name, latest_uber_job)
+        def compare_run_rbe     = get_latest_run(it + compare_rbe_job_name, latest_uber_job)
+        def compare_run_sandbox = get_latest_run(it + compare_sandbox_job_name, latest_uber_job)
+        def bazel_run_run       = get_latest_run(it + bazel_run_job_name, latest_uber_job)
+        def migrate_run         = get_latest_run(it + migrate_job_name, latest_uber_job)
+        def bazel_remote_run    = get_latest_run(it + remote_job_name, latest_uber_job)
+        def bazel_sandbox_run   = get_latest_run(it + sandbox_job_name, latest_uber_job)
 
         def migrated = false
         def compiled = false
         def bazel_run = false
         def compare = false
+        def compare_rbe = false
+        def compare_sandbox = false
 
         if (compare_run != null) {
-            def log = compare_run.log
-            def match = log =~ />>>> Total Maven Test Cases: (\d+)/
+            def match = compare_run.log =~ />>>> Total Maven Test Cases: (\d+)/
             if (match.size() > 0) {
                 maven_tests = match[0][1].toInteger()
             }
-            match = log =~ /> bazel cases: (\d+)/
-            if (match.size() > 0) {
-                total_bazel += match[0][1].toInteger()
-            }
-            match = log =~ /> same: (\d+), different: (\d+), missing: (\d+), overridden: (\d+), ignored: (\d+), failed: (\d+)/
-            if (match.size() > 0) {
-                compare_same       += match[0][1].toInteger()
-                compare_different  += match[0][2].toInteger()
-                compare_missing    += match[0][3].toInteger()
-                compare_overridden += match[0][4].toInteger()
-                compare_ignored    += match[0][5].toInteger()
-                compare_failed     += match[0][6].toInteger()
-            }
+
+            def data = extract_compare_data(compare_run.log)
+            total_bazel        += data.bazel_tests
+            compare_same       += data.compare_same
+            compare_different  += data.compare_different
+            compare_missing    += data.compare_missing
+            compare_overridden += data.compare_overridden
+            compare_ignored    += data.compare_ignored
+            compare_failed     += data.compare_ignored
 
             if (compare_run.result == Result.SUCCESS) {
                 compare_success += 1
@@ -94,6 +119,47 @@ node {
                 compare_fail += 1
         } else {
             compare_never_run += 1
+        }
+
+        // ugh, deduplicate this
+        if (compare_run_rbe != null) {
+            def data = extract_compare_data(compare_run_rbe.log)
+            total_bazel_rbe        += data.bazel_tests
+            compare_same_rbe       += data.compare_same
+            compare_different_rbe  += data.compare_different
+            compare_missing_rbe    += data.compare_missing
+            compare_overridden_rbe += data.compare_overridden
+            compare_ignored_rbe    += data.compare_ignored
+            compare_failed_rbe     += data.compare_ignored
+
+            if (compare_run_rbe.result == Result.SUCCESS) {
+                compare_success_rbe += 1
+                compare_rbe = true
+            }
+            else
+                compare_fail_rbe += 1
+        } else {
+            compare_never_run_rbe += 1
+        }
+
+        if (compare_run_sandbox != null) {
+            def data = extract_compare_data(compare_run_sandbox.log)
+            total_bazel_sandbox        += data.bazel_tests
+            compare_same_sandbox       += data.compare_same
+            compare_different_sandbox  += data.compare_different
+            compare_missing_sandbox    += data.compare_missing
+            compare_overridden_sandbox += data.compare_overridden
+            compare_ignored_sandbox    += data.compare_ignored
+            compare_failed_sandbox     += data.compare_ignored
+
+            if (compare_run_sandbox.result == Result.SUCCESS) {
+                compare_success_sandbox += 1
+                compare_sandbox = true
+            }
+            else
+                compare_fail_sandbox += 1
+        } else {
+            compare_never_run_sandbox += 1
         }
 
         if (bazel_run_run != null) {
@@ -165,10 +231,10 @@ node {
     | -----
     | TEST TOTALS (# of maven tests)
     | - # Maven     = ${total_maven} [that ran in a project]
-    | - # Migrated  = ${(migrated_tests*100)/total_maven}% ${migrated_tests} [ran in a migrated project]
-    | - # Compiled  = ${(compiled_tests*100)/total_maven}% ${compiled_tests} [project which is compiling in bazel]
-    | - # Passing   = ${(passing_tests*100)/total_maven}% ${passing_tests} [project whose tests are passing in bazel]
-    | - # Compared  = ${(compared_tests*100)/total_maven}% ${compared_tests} [project that passed comparison with maven]
+    | - # Migrated  = ${round((migrated_tests*100)/total_maven)}% ${migrated_tests} [ran in a migrated project]
+    | - # Compiled  = ${round((compiled_tests*100)/total_maven)}% ${compiled_tests} [project which is compiling in bazel]
+    | - # Passing   = ${round((passing_tests*100)/total_maven)}% ${passing_tests} [project whose tests are passing in bazel]
+    | - # Compared  = ${round((compared_tests*100)/total_maven)}% ${compared_tests} [project that passed comparison with maven]
     | ---- Total bazel tests: ${total_bazel}
     | ----   of them, passing: ${total_bazel - compare_failed}
     | ----   of them, failed: ${compare_failed}
@@ -178,17 +244,54 @@ node {
     | - unstable = ${remote_run_unstable}
     | - failure = ${remote_run_fail}
     | - not-run = ${remote_run_never_run}
+    | ---- Total bazel tests: ${total_bazel_rbe}
+    | ----   of them, passing: ${total_bazel_rbe - compare_failed_rbe}
+    | ----   of them, failed: ${compare_failed_rbe}
     |--
     |SANDBOX
     | - success = ${sandbox_run_success}
     | - unstable = ${sandbox_run_unstable}
     | - failure = ${sandbox_run_fail}
     | - not-run = ${sandbox_run_never_run}
+    | ---- Total bazel tests: ${total_bazel_sandbox}
+    | ----   of them, passing: ${total_bazel_sandbox - compare_failed_sandbox}
+    | ----   of them, failed: ${compare_failed_sandbox}
     |```""".stripMargin()
 
     echo res
     slackSend channel: "#bazel-mig-reports", message: res
 
+}
+
+@NonCPS
+extract_compare_data(log) {
+    def result = [
+        bazel_tests: 0,
+        compare_same:       0,
+        compare_different:  0,
+        compare_missing:    0,
+        compare_overridden: 0,
+        compare_ignored:    0,
+        compare_failed:     0,
+    ]
+    def match = log =~ /> bazel cases: (\d+)/
+    if (match.size() > 0) {
+        result.bazel_tests = match[0][1].toInteger()
+    }
+    match = log =~ /> same: (\d+), different: (\d+), missing: (\d+), overridden: (\d+), ignored: (\d+), failed: (\d+)/
+    if (match.size() >= 5) {
+        result.compare_same       = match[0][1].toInteger()
+        result.compare_different  = match[0][2].toInteger()
+        result.compare_missing    = match[0][3].toInteger()
+        result.compare_overridden = match[0][4].toInteger()
+        result.compare_ignored    = match[0][5].toInteger()
+        result.compare_failed     = match[0][6].toInteger()
+    }
+    return result
+}
+
+def round(amt) {
+    amt.setScale(2, BigDecimal.ROUND_HALF_UP)
 }
 
 @NonCPS
