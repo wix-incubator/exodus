bfd2fa59a0765946a6aae63a41a2db73fafa77a2
Alon Bar Lev <barlevalon@users.noreply.github.com>
2018-10-07 14:55:40 +0300

    Clearer tests for docker image deduping (#322)
    GitOrigin-RevId: c837e9a1f8ddf7af3ab8bd68c25117635133f18d

--- migrator/wix-bazel-migrator/src/test/scala/com/wix/bazel/migrator/DockerImagesWriterTest.scala
+++ migrator/wix-bazel-migrator/src/test/scala/com/wix/bazel/migrator/DockerImagesWriterTest.scala
@@ -16,22 +16,27 @@ class DockerImagesWriterTest extends SpecificationWithJUnit {
     }
 
     val rootfs: Path = MemoryFileSystemBuilder.newLinux().build().getPath("repo-root")
-    val overrideWithDockerImages = InternalTargetOverride("some-label", dockerImagesDeps = Option(List("mysql:5.7", "docker-repo.wixpress.com/com.wixpress.whatever/whatever:1.234.5", "mysql:5.7")))
-    val overrides: InternalTargetsOverrides = InternalTargetsOverrides(Set(overrideWithDockerImages))
-    new DockerImagesWriter(rootfs, overrides).write()
+    val overrideWithDockerImages = InternalTargetOverride("some-label", dockerImagesDeps = Option(List("mysql:5.7", "docker-repo.wixpress.com/com.wixpress.whatever/whatever:1.234.5")))
+    def overrides: Set[InternalTargetOverride]
+    def writer = new DockerImagesWriter(rootfs, InternalTargetsOverrides(overrides))
   }
 
   "DockerImagesWriter" should {
     "create docker_images.bzl in third_party/docker_images" in new ctx {
+      def overrides: Set[InternalTargetOverride] = Set.empty
+      writer.write()
       Files.exists(rootfs.resolve("third_party/docker_images/docker_images.bzl")) should beTrue
     }
 
     "create BUILD.bazel file in third_party/docker_images" in new ctx {
+      def overrides: Set[InternalTargetOverride] = Set.empty
+      writer.write()
       Files.exists(rootfs.resolve("third_party/docker_images/BUILD.bazel")) should beTrue
     }
 
     "fill default values in container_pull for short-form image" in new ctx {
-
+      def overrides: Set[InternalTargetOverride] = Set(overrideWithDockerImages)
+      writer.write()
       val expected: String =
         s"""|  container_pull(
             |    name = "mysql_5.7",
@@ -39,11 +44,12 @@ class DockerImagesWriterTest extends SpecificationWithJUnit {
             |    repository = "library/mysql",
             |    tag = "5.7"
             |  )""".stripMargin
-
       File(rootfs.resolve("third_party/docker_images/docker_images.bzl")).contentAsString must contain(expected)
     }
 
     "write values as-is in container_pull for full form image" in new ctx {
+      def overrides: Set[InternalTargetOverride] = Set(overrideWithDockerImages)
+      writer.write()
       val expected: String =
         s"""|  container_pull(
             |    name = "com.wixpress.whatever_whatever_1.234.5",
@@ -51,11 +57,12 @@ class DockerImagesWriterTest extends SpecificationWithJUnit {
             |    repository = "com.wixpress.whatever/whatever",
             |    tag = "1.234.5"
             |  )""".stripMargin
-
       File(rootfs.resolve("third_party/docker_images/docker_images.bzl")).contentAsString must contain(expected)
     }
 
     "write container_image in BUILD file" in new ctx {
+      def overrides: Set[InternalTargetOverride] = Set(overrideWithDockerImages)
+      writer.write()
       val expected: String =
         s"""container_image(name="com.wixpress.whatever_whatever_1.234.5", base="@com.wixpress.whatever_whatever_1.234.5//image")""".stripMargin
 
@@ -63,19 +70,18 @@ class DockerImagesWriterTest extends SpecificationWithJUnit {
     }
 
     "deduplicate images in BUILD file" in new ctx {
-
-      val duplicatedImage = "container_image(name=\"mysql_5.7\","
+      def overrides = Set(overrideWithDockerImages, overrideWithDockerImages.copy(label = "duplicate"))
+      writer.write()
       private val fileContent: String = File(rootfs.resolve("third_party/docker_images/BUILD.bazel")).contentAsString
-
-      fileContent must containExactlyOnce(duplicatedImage)
+      fileContent must containExactlyOnce("container_image(name=\"mysql_5.7\",")
     }
 
     "deduplicate images in docker_images.bzl file" in new ctx {
 
-      val duplicatedImage = "name = \"mysql_5.7\","
+      def overrides = Set(overrideWithDockerImages, overrideWithDockerImages.copy(label = "duplicate"))
+      writer.write()
       private val fileContent: String = File(rootfs.resolve("third_party/docker_images/docker_images.bzl")).contentAsString
-
-      fileContent must containExactlyOnce(duplicatedImage)
+      fileContent must containExactlyOnce("name = \"mysql_5.7\",")
     }
   }
 }
