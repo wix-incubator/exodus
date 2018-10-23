68b1d9c7539201af501e27a6f28f14cad10624c9
Elad Weinson (Urson) <38426142+EladWeinson@users.noreply.github.com>
2018-10-23 16:31:15 +0300

    Current branch override for 2nd party scripts (#789)
    * Current branch override for 2nd party scripts
    
    GitOrigin-RevId: 1605857596ebce92da02397752e19db762142741

--- workspaces-resolution/src/main/resolve_2nd_party_repositories.py
+++ workspaces-resolution/src/main/resolve_2nd_party_repositories.py
@@ -30,47 +30,119 @@ tools_relative_path = "/tools/"
 starlark_file_name_postfix = "_2nd_party_resolved_dependencies.bzl"
 json_file_name_postfix = "_2nd_party_resolved_dependencies.json"
 symlink_relative_path = tools_relative_path + "2nd_party_resolved_dependencies_current_branch.bzl"
-second_party_resolved_dependencies_empty_placeholder = "EMPTY!"
+second_party_resolved_deps_override_empty_placeholder = "EMPTY!"
+build_branch_override_empty_placeholder = "~EMPTY~"
 second_party_resolved_dependencies_env_var_name = "SECOND_PARTY_RESOLVED_DEPENDENCIES"
 
-repo_list = os.environ.get("REPO_LIST", "default")
-tracking_branch = os.environ.get("TRACKING_BRANCH", "master")
-second_party_resolved_dependencies = os.environ.get(second_party_resolved_dependencies_env_var_name)
 
-repositories_url = os.environ.get("REPOSITORIES_URL", "https://bo.wix.com/bazel-repositories-server/repositories")
-url_with_params = repositories_url + (
-    "?list={repo_list}&branch={tracking_branch}".format(repo_list=repo_list, tracking_branch=tracking_branch))
+def main():
+    print("Resolving 2nd party dependencies")
+    load_environment_variables()
+    parse_workspace_dir()
+    resolve_repositories()
 
 
-def fetch_repositories(suppress_prints):
-    logging.debug('second_party_resolved_dependencies env var = %s' % second_party_resolved_dependencies)
-    if (second_party_resolved_dependencies is None) or (second_party_resolved_dependencies == second_party_resolved_dependencies_empty_placeholder):
-        logging.debug("Fetching resolved dependencies from url:\t%s" % url_with_params)
-        try:
-            dependencies_raw_string = urlopen(url_with_params, timeout=5).read()
-        except URLError as e:
-            logging.debug("FAIL: fetching from BRS. URLError = %s", e)
-            if not suppress_prints:
-                print("Fetching from bazel repositories server failed (URLError)")
-            sys.exit(1)
-        except socket.timeout:
-            if not suppress_prints:
-                print("Fetching from bazel repositories server failed (Timeout)")
-            sys.exit(1)
+def parse_workspace_dir():
+    global workspace_dir, should_suppress_prints
+    parser = argparse.ArgumentParser()
+    parser.add_argument('workspace_dir')
+    parser.add_argument('suppress_prints')
+    args = parser.parse_args()
+    workspace_dir = args.workspace_dir
+    should_suppress_prints = args.suppress_prints == 'True'
+
+
+def resolve_repositories():
+    starlark_file_path = create_starlark_file_path(read_current_branch())
+    if can_use_existing_resolved_dependencies(starlark_file_path):
+        write_symlink_to_path(symlink_path(), starlark_file_path)
+        if should_suppress_prints:
+            print("2nd party dependencies resolved! (by using a local dependencies file)")
+    elif (second_party_resolved_deps_override is not None) and \
+            (second_party_resolved_deps_override != second_party_resolved_deps_override_empty_placeholder):
+        create_version_files_from_deps_override(second_party_resolved_deps_override)
+        if should_suppress_prints:
+            print("2nd party dependencies resolved! (by resolved dependencies override)")
+    elif (build_branch_override is not None) and \
+            (build_branch_override != build_branch_override_empty_placeholder) and \
+            does_non_empty_file_exist(create_starlark_file_path(build_branch_override)):
+        create_versions_files_from_other_branch(read_current_branch(), build_branch_override)
+        print("2nd party dependencies resolved! (by using build branch override)")
+        if should_suppress_prints:
+            print("2nd party dependencies resolved! (by using build branch override)")
     else:
-        logging.debug("Overriding resolved dependencies from env var SECOND_PARTY_RESOLVED_DEPENDENCIES")
-        dependencies_raw_string = base64.b64decode(second_party_resolved_dependencies)
-        if not suppress_prints:
-            print("Using 2nd party dependencies from environment variable %s" % second_party_resolved_dependencies_env_var_name)
-    io = StringIO()
-    json.dump(json.loads(dependencies_raw_string)["repositories"], io, sort_keys=True, indent=4, separators=(',', ': '))
-    json_file_repos = io.getvalue()
-    starlark_file_repos = json.loads(dependencies_raw_string)["resolvedDependenciesFile"]
-    return json_file_repos, starlark_file_repos
+        create_versions_files_from_server()
+        if should_suppress_prints:
+            print("2nd party dependencies resolved! (by fetching from bazel repositories server)")
 
 
-def file_is_not_empty(some_file):
-    return os.stat(some_file).st_size != 0
+def can_use_existing_resolved_dependencies(starlark_file_path):
+    return does_non_empty_file_exist(starlark_file_path)
+
+
+def can_use_second_party_resolved_depts_override():
+    return (second_party_resolved_deps_override is not None) and \
+           (second_party_resolved_deps_override != second_party_resolved_deps_override_empty_placeholder)
+
+
+def can_use_branch_override():
+    return (build_branch_override is not None) and \
+           (build_branch_override != build_branch_override_empty_placeholder) and \
+           does_non_empty_file_exist(create_starlark_file_path(build_branch_override))
+
+
+def create_version_files_from_deps_override(encoded_deps):
+    logging.debug("Overriding resolved dependencies from env var SECOND_PARTY_RESOLVED_DEPENDENCIES")
+    create_version_files_from_raw_string(base64.b64decode(encoded_deps))
+
+
+def create_versions_files_from_other_branch(current_branch, other_branch):
+    logging.debug("Overriding build branch %s from env var BUILD_BRANCH_OVERRIDE" % other_branch)
+    run_process(['cp', create_starlark_file_path(other_branch),
+                 create_starlark_file_path(current_branch)],
+                'Failed to create resolved dependencies file for branch %s' % current_branch)
+    write_symlink_to_path(symlink_path(), create_starlark_file_path(other_branch))
+
+
+def create_versions_files_from_server():
+    url_with_params = repositories_url + (
+        "?list={repo_list}&branch={tracking_branch}".format(repo_list=repo_list, tracking_branch=tracking_branch))
+    logging.debug("Fetching resolved dependencies from url:\t%s" % url_with_params)
+    try:
+        dependencies_raw_string = urlopen(url_with_params, timeout=5).read()
+    except URLError as e:
+        logging.debug("FAIL: fetching from BRS. URLError = %s", e)
+        if not should_suppress_prints:
+            print("Fetching from bazel repositories server failed (URLError)")
+        sys.exit(1)
+    except socket.timeout:
+        if not should_suppress_prints:
+            print("Fetching from bazel repositories server failed (Timeout)")
+        sys.exit(1)
+    create_version_files_from_raw_string(dependencies_raw_string)
+
+
+def create_version_files_from_raw_string(dependencies_raw_string):
+    json_file_repos, starlark_file_repos = decode_dependencies_to_files(dependencies_raw_string)
+    create_version_files(read_current_branch(), json_file_repos, starlark_file_repos)
+
+
+def create_version_files(current_branch, json_file_repos, starlark_file_repos):
+    json_file_path = "{}{}{}{}".format(workspace_dir, tools_relative_path, current_branch, json_file_name_postfix).replace("\n", "")
+    starlark_file_path = create_starlark_file_path(current_branch)
+
+    write_content_to_path(json_file_repos, json_file_path)
+    write_content_to_path(starlark_file_repos, starlark_file_path)
+    write_symlink_to_path(symlink_path(), starlark_file_path)
+
+    logging.debug("Generating %s" % workspace_dir + "/BUILD.bazel")
+    open(workspace_dir + "/BUILD.bazel", 'a').close()
+    logging.debug("Generating %s" % workspace_dir + "/tools/BUILD.bazel")
+    open(workspace_dir + "/tools/BUILD.bazel", 'a').close()
+
+
+def does_non_empty_file_exist(path):
+    return os.path.isfile(path) and os.stat(path).st_size != 0
 
 
 def read_current_repo_url():
@@ -82,8 +154,8 @@ def read_current_branch():
     return run_process(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], "Failed to read the current branch")
 
 
-def write_symlink_to_path(symlink_path, path):
-    run_process(['ln', '-sf', path, symlink_path], "Failed to write symlink %s => %s" % (symlink_path, path))
+def write_symlink_to_path(link_path, path):
+    run_process(['ln', '-sf', path, link_path], "Failed to write symlink %s => %s" % (link_path, path))
 
 
 def run_process(splitted_command, fail_msg):
@@ -106,46 +178,35 @@ def write_content_to_path(content, path):
         openedfile.close()
 
 
-def write_repositories(workspace_dir, suppress_logs):
-    current_branch = read_current_branch()
-    starlark_file_path = "{}{}{}{}".format(workspace_dir, tools_relative_path, current_branch, starlark_file_name_postfix).replace(
+def load_environment_variables():
+    global repo_list, tracking_branch, second_party_resolved_deps_override, build_branch_override, repositories_url
+    repo_list = os.environ.get("REPO_LIST", "default")
+    logging.debug("[env var] repo_list = %s", repo_list)
+    tracking_branch = os.environ.get("TRACKING_BRANCH", "master")
+    logging.debug("[env var] tracking_branch = %s", tracking_branch)
+    second_party_resolved_deps_override = os.environ.get(second_party_resolved_dependencies_env_var_name)
+    logging.debug("[env var] second_party_resolved_deps_override = %s", second_party_resolved_deps_override)
+    build_branch_override = os.environ.get("BUILD_BRANCH_OVERRIDE")
+    logging.debug("[env var] build_branch_override = %s", build_branch_override)
+    repositories_url = os.environ.get("REPOSITORIES_URL", "https://bo.wix.com/bazel-repositories-server/repositories")
+    logging.debug("[env var] repositories_url = %s", repositories_url)
+
+
+def create_starlark_file_path(branch):
+    return "{}{}{}{}".format(workspace_dir, tools_relative_path, branch, starlark_file_name_postfix).replace(
         "\n", "")
-    symlink_path = workspace_dir + symlink_relative_path
-    if os.path.isfile(starlark_file_path) and file_is_not_empty(starlark_file_path):
-        write_symlink_to_path(symlink_path, starlark_file_path)
-        if not suppress_logs:
-            print("Using 2nd party dependencies from a local versions file (%s)" % starlark_file_path)
-        sys.exit(0)
-
-    json_file_repos, starlark_file_repos = fetch_repositories(suppress_logs)
-
-    json_file_path = "{}{}{}{}".format(workspace_dir, tools_relative_path, current_branch, json_file_name_postfix).replace("\n", "")
-    write_content_to_path(json_file_repos, json_file_path)
-    write_content_to_path(starlark_file_repos, starlark_file_path)
-    write_symlink_to_path(symlink_path, starlark_file_path)
-
-    logging.debug("Generating %s" % workspace_dir + "/BUILD.bazel")
-    open(workspace_dir + "/BUILD.bazel", 'a').close()
-    logging.debug("Generating %s" % workspace_dir + "/tools/BUILD.bazel")
-    open(workspace_dir + "/tools/BUILD.bazel", 'a').close()
-    if not suppress_logs:
-        print("2nd party dependencies resolved!")
 
 
-def parse_workspace_dir():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('workspace_dir')
-    parser.add_argument('suppress_logs')
-    args = parser.parse_args()
-    return args.workspace_dir, args.suppress_logs
+def symlink_path():
+    return workspace_dir + symlink_relative_path
 
 
-def main():
-    (workspace_dir, suppress_logs_string) = parse_workspace_dir()
-    suppress_logs = suppress_logs_string == 'True'
-    if not suppress_logs:
-        print("Resolving 2nd party dependencies...")
-    write_repositories(workspace_dir, suppress_logs)
+def decode_dependencies_to_files(dependencies_raw_string):
+    io = StringIO()
+    json.dump(json.loads(dependencies_raw_string)["repositories"], io, sort_keys=True, indent=4, separators=(',', ': '))
+    json_file_repos = io.getvalue()
+    starlark_file_repos = json.loads(dependencies_raw_string)["resolvedDependenciesFile"]
+    return json_file_repos, starlark_file_repos
 
 
 if __name__ == "__main__":
