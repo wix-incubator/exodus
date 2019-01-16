3ccb29b9fd01401ecd1d4cede13f72cad1ea23d6
Elad Weinson (Urson) <38426142+EladWeinson@users.noreply.github.com>
2019-01-16 13:52:11 +0200

    [sync fixed versions with CI script] - fetching versions from BRS in case they are not already there (#1187)
    Git CLI infra - support for multiple env vars and ad-hoc env vars #automerge
    
    GitOrigin-RevId: 5d8b7c3ee44ca9be978f046910e31507b8cc80ea

--- workspaces-resolution/src/main/resolve_2nd_party_repositories.py
+++ workspaces-resolution/src/main/resolve_2nd_party_repositories.py
@@ -1,37 +1,15 @@
 import argparse
-import json
 import os.path
-import sys
 import base64
-import socket
+
 import zlib
+import sys
+sys.dont_write_bytecode = True
 from utils import *
 
-try:
-    # python 2
-    from StringIO import StringIO
-except ImportError:
-    # python 3
-    from io import StringIO
 
-if sys.version_info[0] == 3:
-    from urllib.request import urlopen
-    from urllib.error import URLError
-else:
-    # Not Python 3 - today, it is most likely to be Python 2
-    # But note that this might need an update when Python 4
-    # might be around one day
-    from urllib2 import urlopen
-    from urllib2 import URLError
-
-tools_relative_path = "/tools/"
-versions_files_local_cache_folder = tools_relative_path + "2nd_party_resolved_dependencies/"
-starlark_file_name_postfix = "_2nd_party_resolved_dependencies.bzl"
-default_json_file_name = "2nd_party_resolved_dependencies.json"
-local_cache_symlink_relative_path = tools_relative_path + "2nd_party_resolved_dependencies_current_branch.bzl"
 second_party_resolved_deps_override_empty_placeholder = "EMPTY!"
 build_branch_override_empty_placeholder = "~EMPTY~"
-json_file_path_override_empty_placeholder = "EMPTY"
 second_party_resolved_dependencies_env_var_name = "SECOND_PARTY_RESOLVED_DEPENDENCIES"
 compressed_second_party_resolved_dependencies_env_var_name = "COMPRESSED_SECOND_PARTY_RESOLVED_DEPENDENCIES"
 json_file_path_override_env_var_name = "RESOLVED_2ND_DEPENDENCIES_JSON_PATH"
@@ -56,7 +34,7 @@ def parse_arguments():
 
 
 def resolve_repositories():
-    local_cache_starlark_file_path = create_local_cache_starlark_file_path(read_current_branch())
+    local_cache_starlark_file_path = create_local_cache_starlark_file_path(workspace_dir, read_current_branch())
     if can_use_existing_resolved_dependencies_from_local_cache(local_cache_starlark_file_path):
         point_local_cache_symlink_to_path(local_cache_starlark_file_path)
     elif (compressed_second_party_resolved_deps_override is not None) and \
@@ -69,20 +47,25 @@ def resolve_repositories():
         point_local_cache_symlink_to_git_tracked_symlink()
     elif (build_branch_override is not None) and \
             (build_branch_override != build_branch_override_empty_placeholder) and \
-            does_non_empty_file_exist(create_local_cache_starlark_file_path(build_branch_override)):
+            does_non_empty_file_exist(create_local_cache_starlark_file_path(workspace_dir, build_branch_override)):
         create_versions_files_from_other_branch(read_current_branch(), build_branch_override)
     else:
-        create_versions_files_from_server()
+        create_versions_files_from_server(workspace_dir,
+                                          repositories_url,
+                                          repo_list,
+                                          tracking_branch,
+                                          should_suppress_prints,
+                                          json_file_path_override)
 
 
 def point_local_cache_symlink_to_path(starlark_file_path):
-    write_symlink_to_path(local_cache_symlink_path(), starlark_file_path)
+    write_symlink_to_path(local_cache_symlink_path(workspace_dir), starlark_file_path)
     if not should_suppress_prints:
         print("2nd party dependencies resolved! (by using a local dependencies file)")
 
 
 def point_local_cache_symlink_to_git_tracked_symlink():
-    write_symlink_to_path(local_cache_symlink_path(), git_tracked_symlink_path())
+    write_symlink_to_path(local_cache_symlink_path(workspace_dir), git_tracked_symlink_path(workspace_dir))
     if not should_suppress_prints:
         print("2nd party dependencies resolved! (by git tracked fixed dependencies)")
 
@@ -92,7 +75,7 @@ def can_use_existing_resolved_dependencies_from_local_cache(starlark_file_path):
 
 
 def can_use_existing_resolved_dependencies_from_git_tracked_folder():
-    return does_non_empty_file_exist(git_tracked_symlink_path())
+    return does_non_empty_file_exist(git_tracked_symlink_path(workspace_dir))
 
 
 def can_use_second_party_resolved_depts_override():
@@ -103,12 +86,12 @@ def can_use_second_party_resolved_depts_override():
 def can_use_branch_override():
     return (build_branch_override is not None) and \
            (build_branch_override != build_branch_override_empty_placeholder) and \
-           does_non_empty_file_exist(create_local_cache_starlark_file_path(build_branch_override))
+           does_non_empty_file_exist(create_local_cache_starlark_file_path(workspace_dir, build_branch_override))
 
 
 def create_version_files_from_deps_override(encoded_deps):
     logging.debug("Overriding resolved dependencies from env var SECOND_PARTY_RESOLVED_DEPENDENCIES")
-    create_version_files_from_raw_string(base64.b64decode(encoded_deps))
+    create_version_files_from_raw_string(base64.b64decode(encoded_deps), workspace_dir, json_file_path_override, should_suppress_prints)
     if not should_suppress_prints:
         print("2nd party dependencies resolved! (by resolved dependencies override)")
 
@@ -117,130 +100,37 @@ def create_version_files_from_compressed_deps_override(encoded_deps):
     logging.debug("Overriding resolved dependencies from env var COMPRESSED_SECOND_PARTY_RESOLVED_DEPENDENCIES")
     decoded_deps = base64.b64decode(encoded_deps)
     uncompressed_deps = zlib.decompress(decoded_deps)
-    create_version_files_from_raw_string(uncompressed_deps)
+    create_version_files_from_raw_string(uncompressed_deps, workspace_dir,json_file_path_override, should_suppress_prints)
     if not should_suppress_prints:
         print("2nd party dependencies resolved! (by compressed resolved dependencies override)")
 
 
 def create_versions_files_from_other_branch(current_branch, other_branch):
     logging.debug("Overriding build branch %s from env var BUILD_BRANCH_OVERRIDE" % other_branch)
-    run_process(['cp', create_local_cache_starlark_file_path(other_branch),
-                 create_local_cache_starlark_file_path(current_branch)],
+    run_process(['cp', create_local_cache_starlark_file_path(workspace_dir, other_branch),
+                 create_local_cache_starlark_file_path(workspace_dir, current_branch)],
                 'Failed to create resolved dependencies file for branch %s' % current_branch)
-    write_symlink_to_path(local_cache_symlink_path(), create_local_cache_starlark_file_path(other_branch))
+    write_symlink_to_path(local_cache_symlink_path(workspace_dir), create_local_cache_starlark_file_path(workspace_dir, other_branch))
     if not should_suppress_prints:
         print("2nd party dependencies resolved! (by using build branch override)")
 
 
-def create_versions_files_from_server():
-    url_with_params = repositories_url + (
-        "?list={repo_list}&branch={tracking_branch}".format(repo_list=repo_list, tracking_branch=tracking_branch))
-    logging.debug("Fetching resolved dependencies from url:\t%s" % url_with_params)
-    try:
-        dependencies_raw_string = urlopen(url_with_params, timeout=5).read()
-    except URLError as e:
-        logging.debug("FAIL: fetching from BRS. URLError = %s", e)
-        if not should_suppress_prints:
-            print("Fetching from bazel repositories server failed (URLError)")
-        sys.exit(1)
-    except socket.timeout:
-        if not should_suppress_prints:
-            print("Fetching from bazel repositories server failed (Timeout)")
-        sys.exit(1)
-    create_version_files_from_raw_string(dependencies_raw_string)
-    if not should_suppress_prints:
-        print("2nd party dependencies resolved! (by fetching from bazel repositories server)")
-
-
-def create_version_files_from_raw_string(dependencies_raw_string):
-    json_file_repos, starlark_file_repos = decode_dependencies_to_files(dependencies_raw_string)
-    create_version_files(read_current_branch(), json_file_repos, starlark_file_repos)
-
-
-def create_version_folder_if_not_exists():
-    versions_folder_full_path = "{}{}".format(workspace_dir, versions_files_local_cache_folder)
-    if not os.path.exists(versions_folder_full_path):
-        logging.debug("Creating folder: {}".format(versions_folder_full_path))
-        os.makedirs(versions_folder_full_path)
-
-
-def create_version_files(current_branch, json_file_repos, starlark_file_repos):
-    starlark_file_path = create_local_cache_starlark_file_path(current_branch)
-    json_file_path = get_json_file_path()
-
-    create_version_folder_if_not_exists()
-    write_content_to_path(json_file_repos, json_file_path)
-    write_content_to_path(starlark_file_repos, starlark_file_path)
-    write_symlink_to_path(local_cache_symlink_path(), starlark_file_path)
-
-    logging.debug("Generating %s" % workspace_dir + "/BUILD.bazel")
-    open(workspace_dir + "/BUILD.bazel", 'a').close()
-    logging.debug("Generating %s" % workspace_dir + "/tools/BUILD.bazel")
-    open(workspace_dir + "/tools/BUILD.bazel", 'a').close()
-
-
-def get_json_file_path():
-    if json_file_path_override is not None and json_file_path_override != json_file_path_override_empty_placeholder:
-        path = "{}/{}".format(workspace_dir, json_file_path_override)
-        logging.debug("Versions used in this build will be written to {}".format(path))
-        if not should_suppress_prints:
-            print("Versions used in this build will be written to {}".format(path))
-        return path
-    return "{}{}{}".format(workspace_dir, versions_files_local_cache_folder, default_json_file_name)
-
-
-def does_non_empty_file_exist(path):
-    return os.path.isfile(path) and os.stat(path).st_size != 0
-
-
-
-
-
-def write_content_to_path(content, path):
-    with open(path, "w+") as opened_file:
-        logging.debug("Generating %s with content:\n%s" % (path, content))
-        opened_file.write(content)
-        opened_file.close()
-
-
 def load_environment_variables():
     global repo_list, tracking_branch, second_party_resolved_deps_override, compressed_second_party_resolved_deps_override, json_file_path_override, build_branch_override, repositories_url
-    repo_list = os.environ.get("REPO_LIST", "default")
+    repo_list = os.environ.get("REPO_LIST", default_repo_list)
     logging.debug("[env var] repo_list = %s", repo_list)
-    tracking_branch = os.environ.get("TRACKING_BRANCH", "master")
+    tracking_branch = os.environ.get("TRACKING_BRANCH", default_tracking_branch)
     logging.debug("[env var] tracking_branch = %s", tracking_branch)
     second_party_resolved_deps_override = os.environ.get(second_party_resolved_dependencies_env_var_name)
     logging.debug("[env var] second_party_resolved_deps_override = %s", second_party_resolved_deps_override)
     compressed_second_party_resolved_deps_override = os.environ.get(compressed_second_party_resolved_dependencies_env_var_name)
     logging.debug("[env var] compressed_second_party_resolved_deps_override = %s", compressed_second_party_resolved_deps_override)
-    json_file_path_override = os.environ.get(json_file_path_override_env_var_name)
+    json_file_path_override = os.environ.get(json_file_path_override_env_var_name, json_file_path_override_empty_placeholder)
     logging.debug("[env var] json_file_path_override = %s", json_file_path_override)
     build_branch_override = os.environ.get("BUILD_BRANCH_OVERRIDE")
     logging.debug("[env var] build_branch_override = %s", build_branch_override)
-    repositories_url = os.environ.get("REPOSITORIES_URL", "https://bo.wix.com/bazel-repositories-server/repositories")
+    repositories_url = os.environ.get("REPOSITORIES_URL", default_repositories_url)
     logging.debug("[env var] repositories_url = %s", repositories_url)
-
-
-def create_local_cache_starlark_file_path(branch):
-    escaped_branch = branch.replace("/", "..")
-    return "{}{}{}{}".format(workspace_dir, versions_files_local_cache_folder, escaped_branch, starlark_file_name_postfix).replace(
-        "\n", "")
-
-
-def local_cache_symlink_path():
-    return workspace_dir + local_cache_symlink_relative_path
-
-
-def git_tracked_symlink_path():
-    return workspace_dir + tools_relative_path + git_tracked_symlink_file_name
-
-
-def decode_dependencies_to_files(dependencies_raw_string):
-    io = StringIO()
-    json.dump(json.loads(dependencies_raw_string)["repositories"], io, sort_keys=True, indent=4, separators=(',', ': '))
-    json_file_repos = io.getvalue()
-    starlark_file_repos = json.loads(dependencies_raw_string)["resolvedDependenciesFile"]
-    return json_file_repos, starlark_file_repos
 
 
 if __name__ == "__main__":
--- workspaces-resolution/src/main/utils.py
+++ workspaces-resolution/src/main/utils.py
@@ -1,11 +1,45 @@
 import logging
 import os
 import subprocess
+import sys
+import socket
+import json
 
-logging_level = logging.DEBUG if "DEBUG_2ND_PARTY_SCRIPT" in os.environ else logging.INFO
+sys.dont_write_bytecode = True
+
+if sys.version_info[0] == 3:
+    from urllib.request import urlopen
+    from urllib.error import URLError
+else:
+    # Not Python 3 - today, it is most likely to be Python 2
+    # But note that this might need an update when Python 4
+    # might be around one day
+    from urllib2 import urlopen
+    from urllib2 import URLError
+
+try:
+    # python 2
+    from StringIO import StringIO
+except ImportError:
+    # python 3
+    from io import StringIOfin
+
+logging_level = logging.DEBUG  # if "DEBUG_2ND_PARTY_SCRIPT" in os.environ else logging.INFO
 logging.basicConfig(level=logging_level, format='%(asctime)s  %(levelname)s: %(message)s')
 
+local_cache_versions_folder = "2nd_party_resolved_dependencies"
+git_tracked_versions_folder = "fixed_2nd_party_resolved_dependencies"
+default_repositories_url = "https://bo.wix.com/bazel-repositories-server/repositories"
+tools_relative_path = "/tools/"
+versions_files_local_cache_folder = tools_relative_path + local_cache_versions_folder + "/"
+starlark_file_name_postfix = "_2nd_party_resolved_dependencies.bzl"
+local_cache_symlink_relative_path = tools_relative_path + "2nd_party_resolved_dependencies_current_branch.bzl"
 git_tracked_symlink_file_name = "fixed_resolved_dependencies_for_ci_branch_build.bzl"
+json_file_path_override_empty_placeholder = "EMPTY"
+default_json_file_name = "2nd_party_resolved_dependencies.json"
+
+default_repo_list = "default"
+default_tracking_branch = "master"
 
 
 def read_current_branch(directory=None):
@@ -32,4 +66,119 @@ def run_process(splitted_command, fail_msg):
 
 def write_symlink_to_path(link_path, path):
     run_process(['ln', '-sf', path, link_path], "Failed to write symlink %s => %s" % (link_path, path))
+
+
+def does_non_empty_file_exist(path):
+    return os.path.isfile(path) and os.stat(path).st_size != 0
+
+
+def git_tracked_symlink_path(workspace_dir):
+    return workspace_dir + tools_relative_path + git_tracked_symlink_file_name
+
+
+def create_versions_files_from_server(workspace_dir,
+                                      repositories_url=default_repositories_url,
+                                      repo_list=default_repo_list,
+                                      tracking_branch=default_tracking_branch,
+                                      should_suppress_prints=True,
+                                      json_file_path_override=json_file_path_override_empty_placeholder):
+    url_with_params = repositories_url + (
+        "?list={repo_list}&branch={tracking_branch}".format(repo_list=repo_list, tracking_branch=tracking_branch))
+    logging.debug("Fetching resolved dependencies from url:\t%s" % url_with_params)
+    try:
+        dependencies_raw_string = urlopen(url_with_params, timeout=5).read()
+    except URLError as e:
+        logging.debug("FAIL: fetching from BRS. URLError = %s", e)
+        if not should_suppress_prints:
+            print("Fetching from bazel repositories server failed (URLError)")
+        sys.exit(1)
+    except socket.timeout:
+        if not should_suppress_prints:
+            print("Fetching from bazel repositories server failed (Timeout)")
+        sys.exit(1)
+    create_version_files_from_raw_string(dependencies_raw_string,
+                                         workspace_dir,
+                                         json_file_path_override,
+                                         should_suppress_prints)
+    if not should_suppress_prints:
+        print("2nd party dependencies resolved! (by fetching from bazel repositories server)")
+
+
+def create_version_files_from_raw_string(dependencies_raw_string,
+                                         workspace_dir,
+                                         json_file_path_override,
+                                         should_suppress_prints):
+    json_file_repos, starlark_file_repos = decode_dependencies_to_files(dependencies_raw_string)
+    create_version_files(workspace_dir, read_current_branch(workspace_dir), json_file_repos, starlark_file_repos,
+                         json_file_path_override, should_suppress_prints)
+
+
+def create_version_files(workspace_dir,
+                         current_branch,
+                         json_file_repos,
+                         starlark_file_repos,
+                         json_file_path_override,
+                         should_suppress_prints):
+    create_starlark_versions_file(workspace_dir, current_branch, starlark_file_repos)
+    create_json_versions_file(workspace_dir, json_file_repos, json_file_path_override, should_suppress_prints)
+
+    logging.debug("Generating %s" % workspace_dir + "/BUILD.bazel")
+    open(workspace_dir + "/BUILD.bazel", 'a').close()
+    logging.debug("Generating %s" % workspace_dir + "/tools/BUILD.bazel")
+    open(workspace_dir + "/tools/BUILD.bazel", 'a').close()
+
+
+def create_starlark_versions_file(workspace_dir, current_branch, starlark_file_repos):
+    starlark_file_path = create_local_cache_starlark_file_path(workspace_dir, current_branch)
+    create_version_folder_if_not_exists(workspace_dir)
+    write_content_to_path(starlark_file_repos, starlark_file_path)
+    write_symlink_to_path(local_cache_symlink_path(workspace_dir), starlark_file_path)
+
+
+def create_json_versions_file(workspace_dir, json_file_repos, json_file_path_override, should_suppress_prints):
+    json_file_path = get_json_file_path(workspace_dir, json_file_path_override, should_suppress_prints)
+    create_version_folder_if_not_exists(workspace_dir)
+    write_content_to_path(json_file_repos, json_file_path)
+
+
+def decode_dependencies_to_files(dependencies_raw_string):
+    io = StringIO()
+    json.dump(json.loads(dependencies_raw_string)["repositories"], io, sort_keys=True, indent=4, separators=(',', ': '))
+    json_file_repos = io.getvalue()
+    starlark_file_repos = json.loads(dependencies_raw_string)["resolvedDependenciesFile"]
+    return json_file_repos, starlark_file_repos
+
+
+def create_local_cache_starlark_file_path(workspace_dir, branch):
+    escaped_branch = branch.replace("/", "..")
+    return "{}{}{}{}".format(workspace_dir, versions_files_local_cache_folder, escaped_branch,
+                             starlark_file_name_postfix).replace("\n", "")
+
+
+def create_version_folder_if_not_exists(workspace_dir):
+    versions_folder_full_path = "{}{}".format(workspace_dir, versions_files_local_cache_folder)
+    if not os.path.exists(versions_folder_full_path):
+        logging.debug("Creating versions folder: {}".format(versions_folder_full_path))
+        os.makedirs(versions_folder_full_path)
+
+
+def write_content_to_path(content, path):
+    with open(path, "w+") as opened_file:
+        logging.debug("Generating %s with content:\n%s" % (path, content))
+        opened_file.write(content)
+        opened_file.close()
+
+
+def local_cache_symlink_path(workspace_dir):
+    return workspace_dir + local_cache_symlink_relative_path
+
+
+def get_json_file_path(workspace_dir, json_file_path_override, should_suppress_prints):
+    if json_file_path_override != json_file_path_override_empty_placeholder:
+        path = "{}/{}".format(workspace_dir, json_file_path_override)
+        logging.debug("Versions used in this build will be written to {}".format(path))
+        if not should_suppress_prints:
+            print("Versions used in this build will be written to {}".format(path))
+        return path
+    return "{}{}{}".format(workspace_dir, versions_files_local_cache_folder, default_json_file_name)
 
