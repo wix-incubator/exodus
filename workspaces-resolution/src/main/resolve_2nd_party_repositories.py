84e9ba79107509d28fbe54084d653b62eabb1339
Elad Weinson (Urson) <38426142+EladWeinson@users.noreply.github.com>
2019-03-07 13:06:30 +0200

    [2nd party scripts] removed code duplication in logging   (#1567)
    * [2nd party scripts] removed code duplication in logging
    #automerge #automerge
    
    * fix bug of missing import
    
    GitOrigin-RevId: a3fc879f02ee89bf18f8a67f9a2d159bf1bd6f13

--- workspaces-resolution/src/main/resolve_2nd_party_repositories.py
+++ workspaces-resolution/src/main/resolve_2nd_party_repositories.py
@@ -6,8 +6,7 @@ import logging
 from python_utils.output_to_shell import OutputToShell, print_message
 from python_utils.logger import set_logging_level
 
-logging_level = logging.DEBUG if "DEBUG_2ND_PARTY_SCRIPT" in os.environ else logging.INFO
-set_logging_level(logging_level)
+set_logging_level()
 sys.dont_write_bytecode = True
 
 
