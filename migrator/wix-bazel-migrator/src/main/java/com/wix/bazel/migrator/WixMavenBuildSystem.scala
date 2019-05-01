63e2522aade77fefe42c85c6e1e96af30dbd2516
Natan Silnitsky <natans@wix.com>
2019-05-01 18:26:45 +0300

    get artifactoryUrls as migrator args (#1894)
    GitOrigin-RevId: 250437f3864c7858c432120813d264d39542ae0c

--- migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/WixMavenBuildSystem.scala
+++ migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/WixMavenBuildSystem.scala
@@ -3,6 +3,4 @@ package com.wix.bazel.migrator
 object WixMavenBuildSystem {
   val RemoteRepoBaseUrl = "repo.dev.wixpress.com:80"
 
-  val RemoteRepo = s"http://$RemoteRepoBaseUrl/artifactory/libs-snapshots"
-  val RemoteRepoReleases = s"http://$RemoteRepoBaseUrl/artifactory/libs-releases"
 }
--- repo-analyzer/maven-repo-analyzer/src/main/java/com/wix/build/maven/analysis/MavenBuildSystem.scala
+++ repo-analyzer/maven-repo-analyzer/src/main/java/com/wix/build/maven/analysis/MavenBuildSystem.scala
@@ -10,11 +10,9 @@ import org.apache.maven.model.io.xpp3.MavenXpp3Reader
 import scala.collection.JavaConverters._
 
 class MavenBuildSystem(repoRoot: Path,
-                       remoteMavenRepositoryUrls: List[String],
                        sourceModulesOverrides: SourceModulesOverrides = SourceModulesOverrides.empty,
-                       maybeDependencyResolver: Option[AetherMavenDependencyResolver] = None) {
+                       dependencyResolver: AetherMavenDependencyResolver) {
 
-  private val dependencyResolver: MavenDependencyResolver = maybeDependencyResolver.getOrElse(new AetherMavenDependencyResolver(remoteMavenRepositoryUrls, ignoreMissingDependenciesFlag = true))
   private val mavenSourceModules: MavenSourceModules = new MavenSourceModules(repoRoot, sourceModulesOverrides)
   def modules(): Set[SourceModule] = {
     mavenSourceModules.modules()
