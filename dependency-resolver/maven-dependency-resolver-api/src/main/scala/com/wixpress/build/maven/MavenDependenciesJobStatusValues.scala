24bb0f069dfba23602f147e281626c138ae27420
Natan Silnitsky <natans@wix.com>
2019-05-10 00:52:08 +0300

    move maven-dependency-resolver-server related code from public modules to private modules #automerge (#1948)
    GitOrigin-RevId: dd916cb91929559062a90f8536042e131a88b9e6

--- dependency-resolver/maven-dependency-resolver-api/src/main/scala/com/wixpress/build/maven/MavenDependenciesJobStatusValues.scala
+++
@@ -1,7 +0,0 @@
-package com.wixpress.build.maven
-
-sealed abstract class JobStatus(val status: String)
-
-case object JobNotFound extends JobStatus("Not Found")
-case object Completed extends JobStatus("Completed")
-
--- dependency-resolver/maven-dependency-resolver/src/main/scala/com/wixpress/build/maven/CalculateClosureTask.scala
+++
@@ -1,6 +0,0 @@
-package com.wixpress.build.maven
-
-import com.wixpress.build.maven.dependency.resolver.api.v1.DependenciesClosureRequest
-import com.wixpress.hoopoe.ids.Guid
-
-case class CalculateClosureTask(jobId: Guid[CalculateClosureTask], request: DependenciesClosureRequest)
