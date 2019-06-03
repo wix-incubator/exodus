bedeb669351520c29bda57902ef197a7967eed2a
Or Shachar <ors@wix.com>
2019-06-03 10:05:57 +0300

    [deps-sync] First try to get sha256 directly from artifactory (#2070)
    GitOrigin-RevId: f316a3e135874819ec737d4150c35d7744c168ac

--- dependency-synchronizer/bazel-deps-synchronizer/src/main/scala/com/wixpress/build/sync/MavenRepoRemoteStorage.scala
+++ dependency-synchronizer/bazel-deps-synchronizer/src/main/scala/com/wixpress/build/sync/MavenRepoRemoteStorage.scala
@@ -6,47 +6,64 @@ import org.apache.commons.codec.digest.DigestUtils
 import org.slf4j.LoggerFactory
 import scalaj.http.{Http, HttpResponse}
 
-import scala.annotation.tailrec
+import scala.collection.parallel.CompositeThrowable
 import scala.util.{Failure, Success, Try}
 
-class MavenRepoRemoteStorage(baseUrls: List[String])extends DependenciesRemoteStorage {
+class MavenRepoRemoteStorage(baseUrls: List[String]) extends DependenciesRemoteStorage {
 
   private val log = LoggerFactory.getLogger(getClass)
 
-  //TODO - optimize all of this! can access the jarUrl.sha256 directly (verify. seems maybe they exist only for our own snapshots and not for 3rd parties?)
   override def checksumFor(node: DependencyNode): Option[String] = {
     val artifact = node.baseDependency.coordinates
-    calculateChecksum(artifact)
+    getChecksum(artifact)
+      .orElse({
+        log.info("Fallback to calculating checksum by downloading the bytes...")
+        calculateChecksum(artifact)
+      })
+      .toOption
   }
 
-  private def calculateChecksum(coordinates: Coordinates): Option[Sha256] = {
-    val sha256 = for {
-      res <- getJarArtifactIOFallbackFor(coordinates, baseUrls)
-      jar <- getBody(res)
-      sha256 <- calculateSha256(jar)
+  private def getChecksum(coordinates: Coordinates): Try[Sha256] = getWithFallback(getArtifactSha256, coordinates)
 
+  private def calculateChecksum(coordinates: Coordinates): Try[Sha256] =
+    for {
+      res <- getWithFallback(getArtifactBytes, coordinates)
+      sha256 <- calculateSha256(res)
     } yield sha256
 
-    sha256.toOption
-  }
-
-  @tailrec
-  private def getJarArtifactIOFallbackFor(coordinates: Coordinates, baseUrls: List[String]): Try[HttpResponse[Array[Byte]]] = {
-
-    if (baseUrls.nonEmpty) {
-      val result = for {
-        response <- getJarArtifactIOFor(coordinates, baseUrls.head)
-        res <- processResponseCode(response, coordinates)
-      } yield res
-
-      result match {
-        case Failure(ex) => getJarArtifactIOFallbackFor(coordinates, baseUrls.drop(1))
-        case _ => result
+  private def getWithFallback[T](getter: (Coordinates, String) => Try[T],
+                                 coordinates: Coordinates): Try[T] = {
+    val attempts = baseUrls.iterator.map(url => getter(coordinates, url))
+    Try {
+      attempts.collectFirst {
+        case Success(e) => e
+      }.getOrElse {
+        val failures = attempts.collect {
+          case Failure(t) => t
+        }
+        log.warn(s"Could not fetch resource: ${failures.map(_.getMessage)}")
+        throw CompositeThrowable(failures.toSet)
       }
-    } else {
-      printAndFail(new RuntimeException(s"""artifact $coordinates not found in any of the given artifactory urls"""))
     }
   }
+
+  private def getArtifactSha256(artifact: Coordinates, baseUrl: String): Try[Sha256] = {
+    val url = s"$baseUrl/${artifact.toSha256Path}"
+    extract(response = Http(url).asString, forArtifact = artifact, inUrl = url)
+  }
+
+  private def getArtifactBytes(artifact: Coordinates, baseUrl: String): Try[Array[Byte]] = {
+    val url = s"$baseUrl/${artifact.toArtifactPath}"
+    extract(response = Http(url).asBytes, forArtifact = artifact, inUrl = url)
+  }
+
+  private def extract[T](response: HttpResponse[T], forArtifact: Coordinates, inUrl: String): Try[T] =
+    response match {
+      case r if r.isSuccess => Success(r.body)
+      case r if r.is4xx => Failure(artifactNotFoundException(forArtifact, inUrl))
+      // TODO: should probably sleep and retry in case of response code 5xx
+      case r => Failure(errorFetchingArtifactException(forArtifact, inUrl, r))
+    }
 
   private def calculateSha256(jar: Array[Byte]) = {
     Try {
@@ -54,40 +71,12 @@ class MavenRepoRemoteStorage(baseUrls: List[String])extends DependenciesRemoteSt
     }
   }
 
-  private def getJarArtifactIOFor(artifact: Coordinates, baseUrl: String) = {
-    val url = s"$baseUrl/${artifact.toArtifactPath}"
-    Try {
-      Http(url).asBytes
-    } match {
-      case Success(response) => Success(response)
-      case Failure(ex) => {
-        log.warn(s"~~~~ ${url}")
-        printAndFail(ex)
-      }
-    }
-  }
 
-  private def processResponseCode[T](response: HttpResponse[T], artifact: Coordinates) = {
-    if (response.code != 404) {
-      Success(response)
-    } else {
-      Failure(new ArtifactNotFoundException(s"artifact: $artifact"))
-    }
-  }
+  private def artifactNotFoundException(artifact: Coordinates, url: String) =
+    ArtifactNotFoundException(s"Could not find sha256 for artifact ${artifact.serialized} in $url")
 
-  private def getBody[T](response: HttpResponse[T]) = {
-    Try {
-      response.body
-    } match {
-      case Success(body) => Success(body)
-      case Failure(ex) => printAndFail(ex)
-    }
-  }
+  private def errorFetchingArtifactException[T](artifact: Coordinates, url: String, r: HttpResponse[T]) =
+    ErrorFetchingArtifactException(s"Error fetching artifact ${artifact.serialized} from $url \n Got response [${r.code} : ${r.statusLine}]")
 
-  private def printAndFail(ex: Throwable) = {
-    log.warn(
-      s"""~~~~ ${ex.getMessage}
-         |""".stripMargin)
-    Failure(ex)
-  }
+
 }
