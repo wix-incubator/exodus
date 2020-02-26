b6f87176cbd22caf167d9a267cdf98f54c68ab4a
Or Shachar <ors@wix.com>
2020-02-26 16:57:05 +0200

    implemented extract JVM Classes

--- tools/JDepsAnalysis/com/wix/jdeps/Main.scala
+++ tools/JDepsAnalysis/com/wix/jdeps/Main.scala
@@ -4,7 +4,7 @@ import java.nio.file.{Path, Paths}
 
 import com.wix.bazel.migrator.model.SourceModule
 import com.wix.build.maven.analysis.{LocalMavenRepository, SourceModules}
-import com.wixpress.build.maven.{AetherMavenDependencyResolver, Dependency}
+import com.wixpress.build.maven.{AetherMavenDependencyResolver, Dependency, MavenScope}
 
 
 case class JVMClass(fqnClass: String,
@@ -16,24 +16,26 @@ case class JVMClass(fqnClass: String,
 }
 
 case class CodePath(module: SourceModule,
-                            relativeSourceDirPathFromModuleRoot: String,
-                            filePath: String) {
+                    relativeSourceDirPathFromModuleRoot: String,
+                    filePath: String) {
   def extension: String = filePath.split('.').last
 }
 
 case class Code(codePath: CodePath, dependencies: List[CodeDependency])
+
 case class CodeDependency(codePath: CodePath, isCompileDependency: Boolean)
 
 case class ClassDependencies(dotFile: Path)
+
 trait JDepsCommand {
-  def analyzeClassesDependenciesPerJar(jarPath: String, classPath: List[String]):ClassDependencies
+  def analyzeClassesDependenciesPerJar(jarPath: String, classPath: List[String]): ClassDependencies
 }
 
 trait JDepsAnalyzer {
-  def analyze(module: SourceModule ): Set[Code]
+  def analyze(module: SourceModule): Set[Code]
 
-//private:
-//  def classToClassDependencies(module: SourceModule ): Map[JVMClass, Set[JVMClass]]
+  //private:
+  //  def classToClassDependencies(module: SourceModule ): Map[JVMClass, Set[JVMClass]]
 
   //  def execute()
 }
@@ -42,42 +44,52 @@ trait JDepsParser {
   def convert(deps: ClassDependencies, relativePathFromMonoRepoRoot: String): Map[JVMClass, Set[JVMClass]]
 }
 
-class JDepsAnalyzerImpl(modules: Set[SourceModule], repoPath: Path) extends JDepsAnalyzer{
-  val jDepsParser:JDepsParser = ???
-  val jDepsCommand:JDepsCommand = new JDepsCommandImpl(repoPath)
+class JDepsAnalyzerImpl(modules: Set[SourceModule], repoPath: Path) extends JDepsAnalyzer {
+  val jDepsParser: JDepsParser = ???
+  val jDepsCommand: JDepsCommand = new JDepsCommandImpl(repoPath)
 
-  def jarPath(module:SourceModule):String =  {
+  def jarPath(module: SourceModule): String = {
     // maybe there's a better way then string manipulation
     module.relativePathFromMonoRepoRoot + s"target/${module.coordinates.artifactId}-${module.coordinates.version}.jar"
   }
 
-  def classesJar(module:SourceModule):String =  {
+  def classesPath(module: SourceModule): String = {
     module.relativePathFromMonoRepoRoot + s"target/classes"
   }
 
-  def testClassesJar(module:SourceModule):String =  {
+  def testClassesPath(module: SourceModule): String = {
     module.relativePathFromMonoRepoRoot + s"target/test-classes"
   }
 
-  def filterRepoModules(deps: Set[Dependency]):Set[SourceModule] = modules.filter(
-    m => deps.map(_.coordinates).contains(m.coordinates)
-  )
+  def filterRepoModules(deps: Set[Dependency], scopes: Set[MavenScope]): Set[SourceModule] = {
+    val relevantDeps = deps.filter(d=>scopes.contains(d.scope))
+    modules.filter(
+      m => relevantDeps.map(_.coordinates).contains(m.coordinates)
+    )
+  }
 
   def convertToCode(map: Map[JVMClass, Set[JVMClass]]): Set[Code] = ???
 
   def extractJvmClasses(module: SourceModule): Map[JVMClass, Set[JVMClass]] = {
-    val a = jDepsCommand.analyzeClassesDependenciesPerJar(jarPath(module),filterRepoModules(module.dependencies.allDependencies).map(jarPath).toList)
-    jDepsParser.convert(a, module.relativePathFromMonoRepoRoot)
+    val jarPath = (filterRepoModules(module.dependencies.allDependencies,Set(MavenScope.Compile)) + module).map(jarPath).toList
+    val productionDeps = jDepsCommand.analyzeClassesDependenciesPerJar(classesPath(module), jarPath)
+   jDepsParser.convert(productionDeps, module.relativePathFromMonoRepoRoot)
+  }
+
+  def extractTestJvmClasses(module: SourceModule): Map[JVMClass, Set[JVMClass]] = {
+    val jarPath = (filterRepoModules(module.dependencies.allDependencies,Set(MavenScope.Compile,MavenScope.Test)) + module).map(jarPath).toList
+    val testDeps = jDepsCommand.analyzeClassesDependenciesPerJar(testClassesPath(module), jarPath)
+    jDepsParser.convert(testDeps, module.relativePathFromMonoRepoRoot)
   }
 
   override def analyze(module: SourceModule): Set[Code] = {
-    val map: Map[JVMClass, Set[JVMClass]] = extractJvmClasses(module)
-    convertToCode(map)
+    val prodMap: Map[JVMClass, Set[JVMClass]] = extractJvmClasses(module)
+    convertToCode(prodMap)
   }
 }
 
 
-object Simulator extends App{
+object Simulator extends App {
   final val user = "natans"
   private val root = s"/Users/$user"
   val localMavenRepository = new LocalMavenRepository(s"$root/.m2/repostiory")
@@ -88,7 +100,7 @@ object Simulator extends App{
   val jDepsAnalyzerImpl = new JDepsAnalyzerImpl(sourceModules, repoRoot)
   sourceModules.foreach(m => {
     val codes = jDepsAnalyzerImpl.analyze(m)
-    println(s">>>> codes: ${codes}")
+    println(s""">>>> codes: $codes""")
   })
 }
 
