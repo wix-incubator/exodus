694f1bb4d2f47884810a5798ddc231c1980bae62
natans <natans@wix.com>
2020-02-27 11:15:02 +0200

    print process output

--- tools/JDepsAnalysis/com/wix/jdeps/JDepsCommandImpl.scala
+++ tools/JDepsAnalysis/com/wix/jdeps/JDepsCommandImpl.scala
@@ -8,16 +8,20 @@ class JDepsCommandImpl(repoRoot: Path) extends JDepsCommand {
     val fileName = Paths.get(jarPath).getFileName.toString
     val dotDirectory = Files.createTempDirectory("dot")
     val classpath = classPath.mkString(":")
-    val process = (new ProcessBuilder).directory(repoRoot.toFile).command(
-      "jdeps",
+    val cmdArgs = List("jdeps",
       "-dotoutput",
       dotDirectory.toString,
       "-v",
       "-cp",
       classpath,
-      jarPath
-    )
-    process.start().waitFor()
+      jarPath)
+    println("jdeps command: " + cmdArgs.mkString(" "))
+    val process = (new ProcessBuilder).directory(repoRoot.toFile).command(cmdArgs:_*)
+    process.redirectOutput()
+    val process1 = process.start()
+    val stream = process1.getInputStream
+    process1.waitFor()
+    println(scala.io.Source.fromInputStream(stream).mkString)
     ClassDependencies(dotDirectory.resolve(fileName + ".dot"))
   }
 }
