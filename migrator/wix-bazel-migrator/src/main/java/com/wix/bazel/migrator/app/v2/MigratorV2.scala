a9f043f208973f0b7d021cd624eb95d9547ece3a
Or Shachar <ors@wix.com>
2020-06-14 17:45:11 +0300

    Changed main to simply instantiate Migrator class

--- migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/app/v2/MigratorV2.scala
+++ migrator/wix-bazel-migrator/src/main/java/com/wix/bazel/migrator/app/v2/MigratorV2.scala
@@ -1,25 +1,21 @@
 package com.wix.bazel.migrator.app.v2
 
 import com.wix.bazel.migrator.analyze.{Code, DependencyAnalyzer}
+import com.wix.bazel.migrator.app.v2.MigratorV2.CodeModules
 import com.wix.bazel.migrator.model
 import com.wix.bazel.migrator.model.{SourceModule, SourceModuleWithoutDeps}
 import com.wix.bazel.migrator.transform.CodeAnalysisTransformer
 
+class MigratorV2(modulesDiscovery: MavenModuleDiscovery,
+                 modulesDepsDiscovery: ModulesDepsDiscovery,
+                 dependencyAnalyzer: DependencyAnalyzer,
+                 externalDepsExtractor: ExternalDepsExtractor,
+                 externalDepsLoader: ExternalDepsLoader,
+                 buildFilesBuilder: BuildFilesBuilder,
+                 staticFiles: StaticFiles,
+                 filesWriter: FilesWriter) {
 
-object MigratorV2 {
-  type CodeModules = Map[SourceModule, List[Code]]
-
-  // all of those members should be instantiated using the run config at the beginning of the run
-  val dependencyAnalyzer: DependencyAnalyzer = ???
-  val modulesDiscovery: MavenModuleDiscovery = ???
-  val modulesDepsDiscovery: ModulesDepsDiscovery = ???
-  val externalDepsExtractor: ExternalDepsExtractor = ???
-  val externalDepsLoader: ExternalDepsLoader = ???
-  val filesWriter: FilesWriter = ???
-  val buildFilesBuilder: BuildFilesBuilder = ???
-  val staticFiles: StaticFiles = ???
-
-  def main(args: Array[String]): Unit = {
+  def migrate(): Unit = {
     // Step 0: find modules with deps
     val modulesWithDeps: Set[SourceModule] = findModulesWithDeps
 
@@ -37,6 +33,7 @@ object MigratorV2 {
     filesWriter.appendToWorkspaceFile(externalDepsLoader.workspacePart)
     staticFiles.otherFiles.foreach(filesWriter.appendToFile)
     filesWriter.appendToWorkspaceFile(staticFiles.worksapceFileFooter)
+
   }
 
   private def writeBuildFile(babelPackage: model.Package): Unit = {
@@ -51,4 +48,32 @@ object MigratorV2 {
 
   // workaround since CodeAnalysisTransformer requires DependencyAnalyzer instance
   private def transformer(codeModules: CodeModules) = new CodeAnalysisTransformer((module: SourceModule) => codeModules(module))
+
+}
+
+object MigratorV2 {
+  type CodeModules = Map[SourceModule, List[Code]]
+
+  // all of those members should be instantiated using the run config at the beginning of the run
+  val modulesDiscovery: MavenModuleDiscovery = ???
+  val modulesDepsDiscovery: ModulesDepsDiscovery = ???
+  val dependencyAnalyzer: DependencyAnalyzer = ???
+  val externalDepsExtractor: ExternalDepsExtractor = ???
+  val externalDepsLoader: ExternalDepsLoader = ???
+  val filesWriter: FilesWriter = ???
+  val buildFilesBuilder: BuildFilesBuilder = ???
+  val staticFiles: StaticFiles = ???
+
+  def main(args: Array[String]): Unit = {
+    val migrator = new MigratorV2(modulesDiscovery,
+      modulesDepsDiscovery,
+      dependencyAnalyzer,
+      externalDepsExtractor,
+      externalDepsLoader,
+      buildFilesBuilder,
+      staticFiles,
+      filesWriter)
+
+    migrator.migrate()
+  }
 }
